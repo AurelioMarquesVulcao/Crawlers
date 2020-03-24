4f9f015066f29056e91274c7147a90df1f278379
Vitor Nascimento <vitor.gan21@gmail.com>
2020-03-24 17:39:03 -0300

    [Comentado] Conexao com o mongo

--- app/server.js
+++ app/server.js
@@ -1,5 +1,6 @@
-require('dotenv/config');
+require('dotenv').config();
 
+const mongoose = require('mongoose');
 const express = require("express");
 
 const port = process.env.API_PORT || "3133";
@@ -9,7 +10,13 @@ app.get("/", (req, res) => {
     res.status(200).send("VAZIO");
 })
 
-app.listen(port, () => {
+// mongoose.connect(
+//     `mongodb://${process.env.MONGO_ROOT_USERNAME}:${process.env.MONGO_ROOT_PASSWORD}@mongodb/admin`,
+//     {useNewUrlParser: true,  useUnifiedTopology: true}
+// );
+
+
+app.listen(3300, () => {
     console.log(`API rodando em: http://localhost:${port}`);
     console.log();
 })

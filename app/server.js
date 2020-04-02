9d3292eaff79705affcfc6525b84dd737f63893e
Vitor Nascimento <vitor.gan21@gmail.com>
2020-04-02 18:44:38 -0300

    Uma simples listagem de total de processos

--- app/server.js
+++ app/server.js
@@ -1,22 +1,29 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-const express = require("express");
+const express = require('express');
 
-const port = process.env.API_PORT || "3133";
+const { Processo } = require('./models/schemas/processo');
+
+const port = process.env.API_PORT || '3133';
 const app = express();
 
-app.get("/", (req, res) => {
-    res.status(200).send("VAZIO");
-})
+app.get('/', (req, res) => {
+  Processo.countDocuments({}, function(err, result) {
+    if (err) {
+      console.log(err);
+      res.status(220).send(err);
+    }
+    res.status(200).send(`${result} Processos cadastrados`);
+  });
+});
 
-// mongoose.connect(
-//     `mongodb://${process.env.MONGO_ROOT_USERNAME}:${process.env.MONGO_ROOT_PASSWORD}@mongodb/admin`,
-//     {useNewUrlParser: true,  useUnifiedTopology: true}
-// );
-
+mongoose.connect(
+  `mongodb://${process.env.MONGO_ROOT_USERNAME}:${process.env.MONGO_ROOT_PASSWORD}@mongodb/admin`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 app.listen(3300, () => {
-    console.log(`API rodando em: http://localhost:${port}`);
-    console.log();
-})
+  console.log(`API rodando em: http://localhost:${port}`);
+  console.log();
+});

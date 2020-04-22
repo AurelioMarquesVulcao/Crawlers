6178628655f52742deb334192b9bbc5d0efed0d8
Vitor Nascimento <vitor.gan21@gmail.com>
2020-04-22 18:32:18 -0300

    Adicionando TJSP ao Factory

--- app/extratores/extratorFactory.js
+++ app/extratores/extratorFactory.js
@@ -1,14 +1,18 @@
-const { OabTJBAPortal } = require('./extratores');
-
+const { OabTJBAPortal, OabTJSP } = require('./extratores');
 class ExtratorFactory {
   static getExtrator(fila, isDebug) {
     let extrator;
 
-    if (/OabTJBAPortal/.test(fila))
+    if (/OabTJBAPortal/.test(fila)) {
       extrator = new OabTJBAPortal(
         'http://www5.tjba.jus.br/portal/busca-resultado',
         isDebug
       );
+    }
+
+    if (/OabTJSP/.test(fila)) {
+      extrator = new OabTJSP('https://esaj.tjsp.jus.br/cpopg/open.do', isDebug);
+    }
 
     return extrator;
   }

16cee56bc4f1c3e07e32a732bb1ed631e43f5780
Vitor Nascimento <vitor.gan21@gmail.com>
2020-04-28 11:43:01 -0300

    Adicionado fila

---
+++ app/workers/TJSP/extracao.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const { enums } = require('../../configs/enums');
+const { GerenciadorFila } = require('../../lib/filaHandler');
+const { ExtratorFactory } = require('../../extratores/extratorFactory');
+const { Extracao } = require('../../models/schemas/extracao');
+const { Helper } = require('../../lib/util');
+
+(async () => {
+  try {
+    mongoose.connect(enums.mongo.address, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    mongoose.connection.on('error', (e) => {
+      console.log(e);
+    });
+
+    const nomeFila = `${enums.tipoConsulta.Oab}${enums.nomesRobos.TJSP}.extracao.novos`;
+
+    new GerenciadorFila().consumir(nomeFila, async (ch, msg) => {
+      const extrator = ExtratorFactory.getExtrator(nomeFila, true);
+      let message = JSON.parse(msg.content.toString());
+      console.log(message);
+      const resultadoExtracao = await extrator.extrair(message.NumeroDaOab);
+      let extracao = await Extracao.criarExtracao(
+        message,
+        resultadoExtracao,
+        'BA'
+      );
+      const resposta = await Helper.enviarFeedback(
+        extracao.prepararEnvio()
+      ).catch((err) => {
+        console.log('Erro detectado', err);
+      });
+      // ch.ack(msg);
+      console.log(resposta);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+})();

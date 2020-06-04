e3394975c2c6fcbe29f9808e98c2500dfd2955dc
Vitor Nascimento <vitor.gan21@gmail.com>
2020-06-04 12:27:14 -0300

    movidas variaveis para fora do try, feito logs quando tendo alguma exception

--- app/workers/TJBAPortal/extracaoNovos.js
+++ app/workers/TJBAPortal/extracaoNovos.js
@@ -25,9 +25,6 @@ const logarExecucao = async (execucao) => {
     new GerenciadorFila().consumir(nomeFila, async (ch, msg) => {
       dataInicio = new Date();
       let message = JSON.parse(msg.content.toString());
-      try{
-      const dataInicio = new Date();
-      //let message = JSON.parse(msg.content.toString());
       let logger = new Logger(
         'info',
         'logs/OabTJBAPortal/OabTJBAPortalInfo.log',
@@ -36,12 +33,14 @@ const logarExecucao = async (execucao) => {
           NumeroDaOab: message.NumeroDaOab,
         }
       );
+      try{
+      const dataInicio = new Date();
       logger.info('Mensagem recebida');
       const extrator = ExtratorFactory.getExtrator(nomeFila, true);
 
       logger.info('Iniciando processo de extração');
       const resultadoExtracao = await extrator.extrair(message.NumeroDaOab);
-
+      logger.logs = [...logger.logs, ...resultadoExtracao.logs];
       //TODO check this part
       logger.info('Processo extraido');
       let extracao = await Extracao.criarExtracao(
@@ -55,28 +54,34 @@ const logarExecucao = async (execucao) => {
       const resposta = await Helper.enviarFeedback(
         extracao.prepararEnvio()
       ).catch((err) => {
+        console.log(err);
         console.log(
           `TJBAPortal - Erro ao enviar resposta ao BigData - Oab: ${message.NumeroDaOab}`
         );
         throw new Error(`TJBAPortal - Erro ao enviar resposta ao BigData - Oab: ${message.NumeroDaOab}`)
-        console.log(err);
       });
       logger.info('Resposta enviada ao BigData');
       logger.info('Reconhecendo mensagem ao RabbitMQ');
       ch.ack(msg);
       logger.info('Mensagem reconhecida');
       logger.info('Finalizando processo');
-      
       await logarExecucao(
         {
-          ...message,
+          Mensagem: message,
           dataInicio: dataInicio,
           dataTermino: new Date(),
           status: 'OK',
+          logs: logger.logs,
           NomeRobo: enums.nomesRobos.TJBAPortal
         }
       );
     } catch (e) {
+      logger.info('Encontrado erro durante a execução');
+      logger.info(`Error: ${e.message}`);
+      logger.info('Reconhecendo mensagem ao RabbitMQ');
+      ch.ack(msg);
+      logger.info('Mensagem reconhecida');
+      logger.info('Finalizando proceso');
       await logarExecucao(
         {
           LogConsultaId: message.LogConsultaId,
@@ -85,10 +90,10 @@ const logarExecucao = async (execucao) => {
           DataTermino: new Date(),
           status: e.message, 
           error: e.stack.replace(/\n+/,' ').trim(),
+          logs: logger.logs,
           NomeRobo: enums.nomesRobos.TJBAPortal
         }
       );
-      console.log(e);
     }
   });
   

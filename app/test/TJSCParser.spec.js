5932e058cfe649862b23404ee5350f16725283a7
Vitor Nascimento <vnascimento@impacta.adv.br>
2020-06-09 19:26:22 -0300

    Elementos de teste de extração utilizando o parser do TJSC

---
+++ app/test/TJSCParser.spec.js
@@ -0,0 +1,51 @@
+const chai = require("chai");
+const subset = require("chai-subset");
+const fs = require("fs");
+const moment = require("moment");
+const { TJSCParser } = require("../parsers/TJSCParser");
+
+const dataAtual = moment().format("YYYY-MM-DD");
+const dataFormatada = new Date(dataAtual).toISOString();
+
+const teste = (numeroProcesso) => {
+  const codigoHtml = fs.readFileSync(
+    `test/testCases/TJSC/${numeroProcesso}.html`
+  );
+  let resposta = fs.readFileSync(
+    `test/testCases/TJSC/resposta_${numeroProcesso}.json`
+  );
+  resposta = JSON.parse(resposta);
+  const extracao = new TJSCParser().parse(codigoHtml);
+
+  it("CAPA", function () {
+    const capa = extracao.processo.capa.toJSON();
+    chai.expect(resposta.capa).to.eql(capa);
+  });
+
+  it("DETALHES", () => {
+    const detalhes = extracao.processo.detalhes.toJSON();
+    chai.expect(resposta.detalhes).to.eql(detalhes);
+  });
+
+  it("ENVOLVIDOS", () => {
+    const envolvidos = extracao.processo.envolvidos.toObject();
+    chai.expect(resultado.envolvidos).to.eql(envolvidos);
+  });
+
+  it("ANDAMENTOS", () => {
+    let andamentos = extracao.andamentos;
+
+    andamentos = andamentos.map((element) => {
+      element = JSON.stringify(element);
+      return JSON.parse(element);
+    });
+
+    chai.expect(resultado.andamentos).to.eql(andamentos);
+  });
+};
+
+describe("TJSC - Teste de Parser", function () {
+  describe("Processo: 03236535520148240023", function () {
+    teste("03236535520148240023");
+  });
+});

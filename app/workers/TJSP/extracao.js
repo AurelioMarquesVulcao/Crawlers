d71b8ee899204d408dae5df8b49990438752a42f
Vitor Nascimento <vitor.gan21@gmail.com>
2020-06-17 18:43:03 -0300

    Modificado worker do tjsp, ainda em processo de refatoração

--- app/workers/TJSP/extracao.js
+++ app/workers/TJSP/extracao.js
@@ -1,42 +1,96 @@
-const mongoose = require('mongoose');
-const { enums } = require('../../configs/enums');
-const { GerenciadorFila } = require('../../lib/filaHandler');
-const { ExtratorFactory } = require('../../extratores/extratorFactory');
-const { Extracao } = require('../../models/schemas/extracao');
-const { Helper } = require('../../lib/util');
+const mongoose = require("mongoose");
+const { enums } = require("../../configs/enums");
+const { GerenciadorFila } = require("../../lib/filaHandler");
+const { ExtratorFactory } = require("../../extratores/extratorFactory");
+const { Extracao } = require("../../models/schemas/extracao");
+const { Helper, Logger } = require("../../lib/util");
+const { LogExecucao } = require('../../lib/logExecucao');
+
+const logarExecucao = async (execucao) => {
+  await LogExecucao.salvar(execucao);
+}
 
 (async () => {
-  try {
-    mongoose.connect(enums.mongo.address, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+  mongoose.connect(enums.mongo.connString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }); 
 
-    mongoose.connection.on('error', (e) => {
-      console.log(e);
-    });
+  mongoose.connection.on("error", (e) => {
+    console.log(e);
+  });
 
-    const nomeFila = `${enums.tipoConsulta.Oab}${enums.nomesRobos.TJSP}.extracao.novos`;
+  const nomeFila = `${enums.tipoConsulta.Oab}.${enums.nomesRobos.TJSP}.extracao.novos`;
 
     new GerenciadorFila().consumir(nomeFila, async (ch, msg) => {
-      const extrator = ExtratorFactory.getExtrator(nomeFila, true);
+      const dataInicio = new Date();
       let message = JSON.parse(msg.content.toString());
-      console.log(message);
+      let logger = new Logger(
+        'info',
+        'logs/OabTJSP/OabTJSPInfo.log',
+        {
+          nomeRobo: enums.nomesRobos.TJSP,
+          NumeroOab: message.NumeroOab,
+        }
+      );
+      try{
+      logger.info('Mensagem recebida');
+      const extrator = ExtratorFactory.getExtrator(nomeFila, true);
+
+      logger.info('Iniciando processo de extração');
       const resultadoExtracao = await extrator.extrair(message.NumeroOab);
+      logger.logs = [...logger.logs, ...resultadoExtracao.logs];
+      logger.info('Processo extraido');
       let extracao = await Extracao.criarExtracao(
         message,
         resultadoExtracao,
         message.SeccionalOab
       );
+      logger.info('Resultado da extracao salva');
+
+      logger.info('Enviando resposta ao BigData');
       const resposta = await Helper.enviarFeedback(
         extracao.prepararEnvio()
       ).catch((err) => {
-        console.log('Erro detectado', err);
+        console.log(err);
+        throw new Error(`OabTJSP - Erro ao enviar resposta ao BigData - Oab: ${message.NumeroOab}`)
       });
-      // ch.ack(msg);
-      console.log(resposta);
-    });
-  } catch (e) {
-    console.log(e);
-  }
+      logger.info('Resposta enviada ao BigData');
+      logger.info('Reconhecendo mensagem ao RabbitMQ');
+      //ch.ack(msg); removerComentario
+      logger.info('Mensagem reconhecida');
+      logger.info('Finalizando processo');
+      await logarExecucao(
+        {
+          Mensagem: message,
+          DataInicio: dataInicio,
+          DataTermino: new Date(),
+          status: 'OK',
+          logs: logger.logs,
+          NomeRobo: enums.nomesRobos.TJSP
+        }
+      );
+    } catch (e) {
+      console.log('ERRU',e.code, e.message, '\n\n', e);
+      logger.info('Encontrado erro durante a execução');
+      logger.info(`Error: ${e.message}`);
+      logger.info('Reconhecendo mensagem ao RabbitMQ');
+      //ch.ack(msg); //TODO remover comentario
+      logger.info('Mensagem reconhecida');
+      logger.info('Finalizando proceso');
+      await logarExecucao(
+        {
+          LogConsultaId: message.LogConsultaId,
+          Mensagem: message, 
+          DataInicio: dataInicio,
+          DataTermino: new Date(),
+          status: e.message, 
+          error: e.stack.replace(/\n+/,' ').trim(),
+          logs: logger.logs,
+          NomeRobo: enums.nomesRobos.TJSP
+        }
+      );
+    }
+  });
+  
 })();

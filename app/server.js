993535a99b2b6c5402cfc7a27f50ee30359d2fef
Newton Gonzaga Costa <ncosta@grupoprazo.adv.br>
2020-06-22 16:02:09 -0300

    finalizacao de api para receber e gravar feedback do bigdata do proadv

--- app/server.js
+++ app/server.js
@@ -2,13 +2,19 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 const express = require('express');
+const bodyParser = require("body-parser");
 
 const { Processo } = require('./models/schemas/processo');
 const { Andamento } = require('./models/schemas/andamento');
 const axios = require('axios');
 
+const routes = require('./api/routes/routes');
+
 const port = process.env.API_PORT || '3133';
 const app = express();
+
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
 
 app.get('/', (req, res) => {
   Processo.countDocuments({}, function (err, result) {
@@ -61,6 +67,8 @@ app.get('/getProcesso', (req, res) => {
   );
 });
 
+app.use("/api", routes);
+
 // `mongodb://${process.env.MONGO_ROOT_USERNAME}:${process.env.MONGO_ROOT_PASSWORD}@mongodb/admin`,
 
 mongoose.connect(  
@@ -68,7 +76,7 @@ mongoose.connect(
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
-app.listen(3300, () => {
+app.listen(port, () => {
   console.log(`API rodando em: http://localhost:${port}`);
   console.log();
 });

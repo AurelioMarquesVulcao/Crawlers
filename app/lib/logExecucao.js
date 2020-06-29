f3213db4baee3d05eb7cd1b2002dca0dbaa1f723
Vitor Nascimento <vitor.gan21@gmail.com>
2020-06-29 18:49:10 -0300

    Atalho para cadastro em nova fila

--- app/lib/logExecucao.js
+++ app/lib/logExecucao.js
@@ -2,9 +2,15 @@ require('../bootstrap');
 const mongoose = require("mongoose");
 const ExecucaoConsulta = require("../models/schemas/execucao_consulta")
   .ExecucaoConsulta;
-
+const GerenciadorFila = require("../lib/filaHandler").GerenciadorFila;
+const { enums } = require('../configs/enums');
 const { Helper } = require('./util');
 
+let mapaEstadoRobo = {
+  BA: enums.nomesRobos.TJBAPortal,
+  SP: enums.nomesRobos.TJSP
+};
+const gf = new GerenciadorFila();
 module.exports.LogExecucao = class LogExecucao {
 
   static async salvar(execucao) {
@@ -27,6 +33,34 @@ module.exports.LogExecucao = class LogExecucao {
       // upsert true, caso precise de um jeito de criar a consulta na hora
       { upsert: true } 
     );
+  }
+
+  static async cadastrarConsultaPendente(consultaPendente) {
+    const nomeRobo = mapaEstadoRobo[consultaPendente.SeccionalOab];
+
+    if (nomeRobo) {
+      const nomeFila = `${consultaPendente.TipoConsulta}.${nomeRobo}.extracao.novos`;
+      const execucao = {
+        ConsultaCadastradaId: consultaPendente._id,
+        NomeRobo: nomeRobo,
+        Log: [
+          {
+            status: `Execução do robô ${nomeRobo} para consulta ${consultaPendente._id} foi cadastrada com sucesso!`
+          }
+        ]
+      };
+      const execucaoConsulta = new ExecucaoConsulta(execucao);
+      const ex = await execucaoConsulta.save();
+      const mensagem = {
+        ExecucaoConsultaId: ex._id,
+        ConsultaCadastradaId: consultaPendente._id,
+        DataEnfileiramento: new Date(),
+        NumeroProcesso: consultaPendente.NumeroProcesso,
+        NumeroOab: consultaPendente.NumeroOab,
+        SeccionalOab: consultaPendente.SeccionalOab
+      };
+      gf.enviar(nomeFila, mensagem);
+    }
   }
   
 }

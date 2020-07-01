682b0ed3037d2d4b0c2a65d5bcb44aa483ee4187
Vitor Nascimento <vitor.gan21@gmail.com>
2020-07-01 13:59:10 -0300

    Atualizado merge com o FIX TJBA com endpoints baseados na .env

--- app/server.js
+++ app/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 
 const { Processo } = require('./models/schemas/processo');
 const { Andamento } = require('./models/schemas/andamento');
+const { enums } = require('./configs/enums');
 const axios = require('axios');
 
 const port = process.env.API_PORT || '3133';
@@ -18,20 +19,6 @@ app.get('/', (req, res) => {
     }
     res.status(200).send(`${result} Processos cadastrados`);
   });
-});
-
-app.get('/teste', async (req, res) => {
-  const options = {
-    url: 'http://www5.tjba.jus.br/portal/',
-    method: 'GET',
-  };
-  axios(options)
-    .then((response) => {
-      res.status(200).send(response.data);
-    })
-    .catch((err) => {
-      res.status(200).send(err);
-    });
 });
 
 app.get('/getProcesso', (req, res) => {
@@ -61,10 +48,17 @@ app.get('/getProcesso', (req, res) => {
   );
 });
 
+app.get('/getAndamentos', (req, res) => {
+  Andamento.retornaAndamentos(req.query.numeroProcesso)
+    .then( andamentos => {
+      res.status(200).send({ processo: req.query.numeroProcesso, andamentos: andamentos });
+    })
+})
+
 // `mongodb://${process.env.MONGO_ROOT_USERNAME}:${process.env.MONGO_ROOT_PASSWORD}@mongodb/admin`,
 
-mongoose.connect(  
-  `mongodb://admin:admin@bigrj01mon01:19000,bigrj01mon02:19000/crawlersBigdata?authSource=admin&replicaSet=rsBigData`,
+mongoose.connect(
+  enums.mongo.connString,
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 

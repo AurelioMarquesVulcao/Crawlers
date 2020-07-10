6d656c9a95c1948076a3fe7c8814fb83b3ef9cd8
aurelio <amarques@impacta.adv.br>
2020-07-10 17:18:44 -0300

    Testes de melhoria de desempenho do robo.

--- app/lib/roboPuppeteer_test.js
+++ app/lib/roboPuppeteer_test.js
@@ -1,16 +1,8 @@
 const puppeteer = require('puppeteer');
 
 
-const processosMG = ['00021625020145020016', '00101972120205030158', '00103665120205030079']
-const prenche = async () => {
-    for (let i = 0; i < processosMG.length; i++) {
-        await // função de preenchimento de processo
-        await console.log('processo : ' + processosMG[i] + ' adicionado');
 
-    }
-}
-
-const processaNumero2 = (numero) => {
+function processaNumero(numero) {
     let numeroProcesso = numero.trim().slice(0, 7);
     let ano = numero.trim().slice(9, 13);
     let vara = numero.trim().slice(numero.length - 4, numero.length);
@@ -21,7 +13,7 @@ const processaNumero2 = (numero) => {
     }
 }
 
-const escolheEstado = (numero) => {
+function escolheEstado(numero) {
     let resultado;
     numero = numero.slice(numero.length - 6, numero.length - 4)
     if (numero == 01) resultado = 2     // Rio de Janeiro
@@ -31,6 +23,200 @@ const escolheEstado = (numero) => {
     if (numero == 03) resultado = 4    // São Paulo
     return resultado
 }
+
+const roboVersao2 = async (numero) => {
+    entrada = processaNumero(numero)
+
+    const slow = numeroAleatorio(200, 350);
+    const delay = numeroAleatorio(10, 40);
+
+
+    // para abrir o navegador use o headless: false
+    var browser = await puppeteer.launch({
+        headless: false,
+        slowMo: slow,
+        ignoreHTTPSErrors: true,
+        //executablePath: '/usr/bin/chromium-browser',
+        // args: ['--ignore-certificate-errors', '--no-sandbox', '--headless', '--disable-gpu']
+        // args para rodar localmente.
+        args: ['--ignore-certificate-errors', '--disable-gpu']
+    });
+    // se tirar isso travará a aplicacao
+    await console.log(`info: JTE - CNJ: ${numero} - Puppeteer foi ativado`);
+
+    var page = await browser.newPage();
+
+
+
+
+    // faz novas tentativas até conseguir logar no tribunal
+    let logaTribunal = async () => {
+        try {
+            await preencheTribunal()
+        } catch (e) {
+            console.log("\033[31m" + `info: JTE - CNJ: ${numero} - ERRO!!! NÃO FOI POSSIVEL ABRIR O TRIBUNAL`);
+            await browser.close()
+            await preencheTribunal()
+
+        }
+    }
+    await logaTribunal()
+
+    async function preencheTribunal() {
+        // deixa a pagina menor a fim de economizar memoria
+        await page.goto('http://jte.csjt.jus.br/', { waitUntil: 'networkidle2' })
+        // se tirar isso travará a aplicacao
+        await console.log(`info: JTE - CNJ: ${numero} - Puppeteer entrou na pagina => https://jte.csjt.jus.br/`);
+        // para esperar carregar o elemento onde fica o tribunal
+        await page.waitFor(900)
+        await page.waitFor('mat-form-field');
+        await page.waitFor(900)
+        await page.waitFor('#mat-select-1 > div > div.mat-select-arrow-wrapper')
+        await page.waitFor(900)
+        await page.click('#mat-select-1 > div > div.mat-select-arrow-wrapper')
+        await page.waitFor(900)
+        await page.click(`#mat-option-${escolheEstado(numero)}`)
+        await page.click('ng-component > div.botoesAcao.mat-dialog-actions > button:nth-child(2) > span')
+        await page.waitFor(900)
+        // await page.waitFor('#consultaProcessual')
+        await page.click('#consultaProcessual')
+    }
+
+
+
+    let preencheProcesso = async () => {
+        await page.waitFor(900)
+        const input1 = await page.$('#campoNumeroProcesso');
+        await input1.click({ clickCount: 3 })
+        await page.type('#campoNumeroProcesso', `${entrada.numeroprocesso}`)
+
+        const input2 = await page.$('#campoAno');
+        await input2.click({ clickCount: 3 })
+
+        await page.type('#campoAno', `${entrada.ano}`)
+
+        const input3 = await page.$('#campoVara');
+        await input3.click({ clickCount: 3 })
+
+        await page.type('#campoVara', `${entrada.vara}`)
+        await page.click('#consulta > ion-row > ion-col:nth-child(1) > mat-card > div > form > ion-button')
+
+        //await page.waitFor('#mat-tab-content-0-0 > div > detalhes-aba-geral > div')
+    }
+
+
+    let buscaProcesso = async (contador) => {
+        try {
+            await preencheProcesso()
+            return await pegaHtml(contador)
+        } catch (e) {
+            // temporariamente estou tentando infinitamente
+            await preencheProcesso()
+        }
+    }
+    const processosMG = ['00021625020145020016', '00101972120205030158', '00103665120205030079']
+    for (i in processosMG) {
+        await buscaProcesso(i)
+    }
+
+
+
+    async function pegaHtml(contador) {
+        //const contador = 0
+
+        await page.waitFor('#listaProcessoEncontrado > mat-tab-group > div')
+
+        console.log(contador);
+
+        await page.waitFor(`#mat-tab-content-${contador}-0 > div > detalhes-aba-geral > div`)
+        // se o processo existir pego os dados gerais.
+        let html1 = await page.evaluate(() => {
+
+            let text = document.querySelector('html').innerHTML;
+            return text
+        })
+        await console.log("\033[0;32m" + `info: JTE - CNJ: ${numero} - html da capa do processo extraido do Puppeteer`);
+
+        const divButon = '#listaProcessoEncontrado > mat-tab-group > mat-tab-header > div.mat-tab-label-container > div > div'
+        // #listaProcessoEncontrado > mat-tab-group > mat-tab-header
+        await page.click(`#mat-tab-label-${contador}-1`)
+        //await page.click(divButon)[0].children[1]
+        await page.waitFor('#divMovBrowser1 > ion-grid > ion-row > ion-col')
+        // pego os andamentos do processo
+        let html2 = await page.evaluate(() => {
+
+            let text = document.querySelector('html').innerHTML;
+            return text
+        })
+        await console.log("\033[0;32m" + `info: JTE - CNJ: ${numero} - html dos andamentos extraido do Puppeteer`);
+        // await page.close()
+        //await browser.close()
+        return { geral: html1, andamentos: html2 }
+    }
+}
+
+function numeroAleatorio(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+roboVersao2("00100210520185150085")
+
+
+
+
+
+module.exports.RoboPuppeteer = roboVersao2
+
+
+
+
+
+
+
+
+
+// -----------------------------------codigos antigos --------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+const prenche = async () => {
+    for (let i = 0; i < processosMG.length; i++) {
+        await // função de preenchimento de processo
+            await console.log('processo : ' + processosMG[i] + ' adicionado');
+
+    }
+}
+
+// const processaNumero2 = (numero) => {
+//     let numeroProcesso = numero.trim().slice(0, 7);
+//     let ano = numero.trim().slice(9, 13);
+//     let vara = numero.trim().slice(numero.length - 4, numero.length);
+//     return {
+//         numeroprocesso: numeroProcesso,
+//         ano: ano,
+//         vara: vara
+//     }
+// }
+
+// const escolheEstado = (numero) => {
+//     let resultado;
+//     numero = numero.slice(numero.length - 6, numero.length - 4)
+//     if (numero == 01) resultado = 2     // Rio de Janeiro
+//     if (numero == 02 || numero == 05) resultado = 03    // São Paulo
+//     if (numero == 21) resultado = 22    // Rio Grande do Norte
+//     if (numero == 15) resultado = 16    // São Paulo
+//     if (numero == 03) resultado = 4    // São Paulo
+//     return resultado
+// }
 
 const roboVersao1 = async (numero) => {
     entrada = processaNumero2(numero)
@@ -99,10 +285,10 @@ const roboVersao1 = async (numero) => {
         }
     }
     await logaTribunal()
-    
+
     //prenche()
-    
-    
+
+
     let preencheProcesso = async () => {
         await page.waitFor(900)
         //await page.waitFor('#consulta > ion-row > ion-col:nth-child(1) > mat-card > div > div')
@@ -163,16 +349,14 @@ function numeroAleatorio(min, max) {
 // roboVersao1("00100210520185150085")
 
 
-const processos = processosmg
+//const processos = processosmg
 
 const post = async () => {
     for (let i = 0; i < processos.length; i++) {
-        await roboVersao1(processos[i])
+        await roboVersao2(processos[i])
         await console.log('processo : ' + processos[i] + ' adicionado');
 
     }
 }
-post()
 
 
-module.exports.RoboPuppeteer = roboVersao1

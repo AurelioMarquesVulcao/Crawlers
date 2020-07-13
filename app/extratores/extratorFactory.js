fdfa91db8c864a45847bc5c2cc13123c51cee3da
Vitor Nascimento <vitor.gan21@gmail.com>
2020-07-13 18:00:14 -0300

    Retirado extratores não mais necessarios

--- app/extratores/extratorFactory.js
+++ app/extratores/extratorFactory.js
@@ -25,25 +25,11 @@ class ExtratorFactory {
     }
 
     if (/oab.TJSC/.test(fila)) {
-      if (/\.1/.test(fila)) // primeira instancia
-        url = 'https://esaj.tjsc.jus.br/cpopg';
-      if (/\.2/.test(fila)) // segunda instancia
-        url = 'https://esaj.tjsc.jus.br/cposgtj';
-      if(/\.tr/.test(fila)) // turmas recursais
-        url = 'https://esaj.tjsc.jus.br/cposg5';
-
-      extrator = new OabTJSC(url, isDebug);
+      extrator = new OabTJSC('https://esaj.tjsc.jus.br/cpopg', isDebug);
     }
 
     if (/processo.TJSC/.test(fila)) {
-      if (/\.1/.test(fila))
-        url = 'https://esaj.tjsc.jus.br/cpopg';
-      if (/\.2/.test(fila))
-        url = 'https://esaj.tjsc.jus.br/cposgtj';
-      if(/\.tr/.test(fila))
-        url = 'https://esaj.tjsc.jus.br/cposg5';
-
-      extrator = new ProcessoTJSC(url, isDebug);
+      extrator = new ProcessoTJSC('https://esaj.tjsc.jus.br/cpopg', isDebug);
     }
 
     return extrator;

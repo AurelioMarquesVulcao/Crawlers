2237e52470851b19c0244bad44eadc65d28953a7
aurelio <amarques@impacta.adv.br>
2020-07-15 13:11:48 -0300

    Criador de fila start

--- app/lib/criaFilaJTE.js
+++ app/lib/criaFilaJTE.js
@@ -1,4 +1,16 @@
-const GerenciadorFila = require('./filaHandler');
+const mongoose = require("mongoose");
+const re = require('xregexp');
+
+const { enums } = require("../configs/enums");
+const { GerenciadorFila } = require("./filaHandler");
+const { ExtratorFactory } = require("../extratores/extratorFactory");
+const { Extracao } = require("../models/schemas/extracao");
+const { Helper, Logger } = require("./util");
+const { LogExecucao } = require("./logExecucao");
+const { Andamento } = require('../models/schemas/andamento');
+const { BaseException, RequestException, ExtracaoException, AntiCaptchaResponseException, } = require('../../models/exception/exception');
+const { ExtratorBase } = require('../extratores/extratores');
+const { JTEParser } = require('../parsers/JTEParser');
 
 
 
@@ -13,10 +25,10 @@ const GerenciadorFila = require('./filaHandler');
       console.log(e);
     });
   
-    // const nomeFila = `${enums.tipoConsulta.Oab}.${enums.nomesRobos.JTE}.extracao.novos`;
+    
     const nomeFila = `${enums.tipoConsulta.Processo}.${enums.nomesRobos.JTE}.extracao.novos-RJ`;
     const reConsumo = `${enums.tipoConsulta.Processo}.${enums.nomesRobos.JTE}.extracao.novos-RJ`;
 
-    new GerenciadorFila().enviar(nomeFila, message);
-    new GerenciadorFila().enviar(reConsumo, message);
+    //new GerenciadorFila().enviar(nomeFila, message);
+    //new GerenciadorFila().enviar(reConsumo, message);
 })()  

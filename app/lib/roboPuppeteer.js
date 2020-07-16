58f27e655cfbb26b5f82609635a689ccca63258c
vulcao <amarques@impacta.adv.br>
2020-07-16 17:04:07 -0300

    Ajuste para melhoria de velociade do robo.

--- app/lib/roboPuppeteer.js
+++ app/lib/roboPuppeteer.js
@@ -1,20 +1,21 @@
 const puppeteer = require('puppeteer');
+const sleep = require('await-sleep')
 
+var timerSleep = 60
 
 class RoboPuppeteer3 {
   async iniciar() {
     // para abrir o navegador use o headless: false
     this.browser = await puppeteer.launch({
-      headless: true,
-      slowMo: 100,
+      headless: false,
+      slowMo: 1,
       ignoreHTTPSErrors: true,
-      // args: ['--ignore-certificate-errors']
-      args: ['--ignore-certificate-errors', '--no-sandbox', '--headless', '--disable-gpu', '--proxy-server=http://proxy-proadv.7lan.net:8181']
+      args: ['--ignore-certificate-errors', '--no-sandbox']
+      //args: ['--ignore-certificate-errors', '--no-sandbox', '--headless', '--disable-gpu', '--proxy-server=http://proxy-proadv.7lan.net:8181']
       //args: ['--ignore-certificate-errors', '--no-sandbox', '--headless', '--disable-gpu']
     });
     this.page = await this.browser.newPage();
     this.acessar('https://www.google.com/');
-    //var contador = 0
     console.log('O Puppeteer foi Iniciado corretamente');
   }
 
@@ -28,10 +29,6 @@ class RoboPuppeteer3 {
       });
       // isso me da o url completo
       //content = await this.page.content();
-      // provisório
-      content = true
-
-
     } catch (e) {
       console.log(e);
       process.exit();
@@ -42,42 +39,45 @@ class RoboPuppeteer3 {
 
 
 
-  async preencheTribunal(numero) {  
+  async preencheTribunal(numero) {
     // await console.log(`foi escolhido o estado numero ${escolheEstado(numero)}`);
     await console.log(`info: JTE - CNJ: ${numero} - Puppeteer carregou a url => https://jte.csjt.jus.br/`);
     // para esperar carregar o elemento onde fica o tribunal
-    await this.page.waitFor(900)
+    await sleep(timerSleep)
     await this.page.waitFor('mat-form-field');
-    await this.page.waitFor(900)
+    await sleep(timerSleep)
     await this.page.waitFor('#mat-select-1 > div > div.mat-select-arrow-wrapper')
-    await this.page.waitFor(900)
+    await sleep(timerSleep)
     // console.log(!! await this.page.waitFor('#mat-select-1 > div > div.mat-select-arrow-wrapper'));
     await this.page.click('#mat-select-1 > div > div.mat-select-arrow-wrapper')
-    await this.page.waitFor(900)
+    await sleep(timerSleep)
     await this.page.click(`#mat-option-${escolheEstado(numero)}`)
+    await sleep(timerSleep)
     await this.page.click('ng-component > div.botoesAcao.mat-dialog-actions > button:nth-child(2) > span')
-    await this.page.waitFor(1200)
+    await sleep(1200)
     // await this.page.waitFor('#consultaProcessual')
     await this.page.click('#consultaProcessual')
+    await sleep(timerSleep)
     await console.log("Logado ao tribunal desejado");
-  
+
   }
 
 
   async preencheProcesso(numero, contador) {
-    console.time("info: JTE - CNJ : ---------------- - tempo para preencher um processo")
+
     let entrada = processaNumero(numero)
     //await console.log("leu a entrada: " + entrada.numeroprocesso);
     await console.log("O contador de processo esta em: " + contador);
 
 
     //await this.page.click('#consultaProcessual')
-    await this.page.waitFor(600)
+    await sleep(timerSleep)
     // console.log('wait 600');
-    
+
     // console.log(!! await this.page.waitFor('#campoNumeroProcesso'));
 
     await this.page.waitFor('#campoNumeroProcesso')
+    await sleep(timerSleep)
     // const input1 = await this.page.$('#campoNumeroProcesso');
     await this.page.click('#campoNumeroProcesso', { clickCount: 3 })
     await this.page.type('#campoNumeroProcesso', `${entrada.numeroprocesso}`)
@@ -91,48 +91,48 @@ class RoboPuppeteer3 {
     await input3.click({ clickCount: 3 })
 
     await this.page.type('#campoVara', `${entrada.vara}`)
+    await sleep(timerSleep)
     await this.page.click('#consulta > ion-row > ion-col:nth-child(1) > mat-card > div > form > ion-button')
     console.log(`Processo ${numero} foi preenchido com sucesso, obtendo dados.`);
 
     //await page.waitFor('#mat-tab-content-0-0 > div > detalhes-aba-geral > div')
-    console.timeEnd("info: JTE - CNJ : ---------------- - tempo para preencher um processo")
+    await sleep(timerSleep)
     return await this.pegaHtml(contador, numero)
   }
 
 
   async pegaHtml(contador, numero) {
-    console.time("info: JTE - CNJ : ---------------- - tempo para obter um processo")
     //const contador = 0
-
+    await sleep(timerSleep)
     await this.page.waitFor('#listaProcessoEncontrado > mat-tab-group > div')
-
-    // console.log(contador);
-
+    await sleep(timerSleep)
     await this.page.waitFor(`#mat-tab-content-${contador}-0 > div > detalhes-aba-geral > div`)
-    // se o processo existir pego os dados gerais.
-    let html1 = await this.page.evaluate(() => {
-
-      let text = document.querySelector('html').innerHTML;
+    await sleep(timerSleep)
+    
+    let html1 = await this.page.evaluate(async () => {
+      let text = await document.querySelector('html').innerHTML;
       return text
     })
+    
+    //let html1 = await this.page.content();
+    
+    await sleep(timerSleep)
     await console.log(`info: JTE - CNJ: ${numero} - html da capa do processo extraido do Puppeteer`);
-
     const divButon = '#listaProcessoEncontrado > mat-tab-group > mat-tab-header > div.mat-tab-label-container > div > div'
-    // #listaProcessoEncontrado > mat-tab-group > mat-tab-header
+    await sleep(timerSleep)
     await this.page.click(`#mat-tab-label-${contador}-1`)
-    //await page.click(divButon)[0].children[1]
+    await sleep(timerSleep)
     await this.page.waitFor('#divMovBrowser1 > ion-grid > ion-row > ion-col')
-    // pego os andamentos do processo
-    let html2 = await this.page.evaluate(() => {
-
-      let text = document.querySelector('html').innerHTML;
+    await sleep(timerSleep)
+    
+    let html2 = await this.page.evaluate(async () => {
+      let text = await document.querySelector('html').innerHTML;
       return text
     })
+    
+    //let html2 = await this.page.content();
+    
     await console.log(`info: JTE - CNJ: ${numero} - html dos andamentos extraido do Puppeteer`);
-    // await page.close()
-    //await browser.close()
-    //contador ++
-    console.timeEnd("info: JTE - CNJ : ---------------- - tempo para obter um processo")
     return { geral: html1, andamentos: html2 }
   }
 

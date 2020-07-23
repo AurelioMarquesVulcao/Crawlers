939e467d79624da8d326339c6aca43c452d0125b
Newton Gonzaga Costa <ncosta@proadv.adv.br>
2020-07-23 18:36:48 -0300

    api para retornar dados pro bigdata

--- app/api/routes/routes.js
+++ app/api/routes/routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const bigdataRoutes = require("./bigdata");
 const feedbackRoutes = require("./feedback");
 const mockRoutes = require("./mock");
 
@@ -6,7 +7,8 @@ router.get('/', (req, res) => {
     res.send('Não há rota padrao para /api');
 });
 
-router.use("/feedback", feedbackRoutes);
-router.use("/mock", mockRoutes);
+router.use('/bigdata', bigdataRoutes);
+router.use('/feedback', feedbackRoutes);
+router.use('/mock', mockRoutes);
 
 module.exports = router;

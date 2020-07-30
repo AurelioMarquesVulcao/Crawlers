ac92529d6da636892a132bac614e4a8edf0f414b
Vitor Nascimento <vnascimento@impacta.adv.br>
2020-07-30 19:23:08 -0300

    ajustando model

--- app/models/schemas/credenciaisAdvogados.js
+++ app/models/schemas/credenciaisAdvogados.js
@@ -1,22 +1,28 @@
 const mongoose = require('mongoose');
-const { Helper } = require("../../lib/util");
+const { Helper } = require('../../lib/util');
 const Schema = mongoose.Schema;
 
-const credenciaisAdvogadosSchema = new Schema({
-  login: {type: String, required: true},
-  senha: {type: String, required: true},
-  estado: {type: String, required: true},
-  _hash: {type: String, required: true, unique: true},
-  nome: String
-}, { versionKey: false });
+const credenciaisAdvogadosSchema = new Schema(
+  {
+    login: { type: String, required: true },
+    senha: { type: String, required: true },
+    estado: { type: String, required: true },
+    _hash: { type: String, required: true, unique: true },
+    nome: String,
+  },
+  { versionKey: false }
+);
 
-credenciaisAdvogadosSchema.statics.getCredenciais = async function getCredenciais(estado, idsUsados=[]) {
+credenciaisAdvogadosSchema.statics.getCredenciais = async function getCredenciais(
+  estado,
+  idsUsados = []
+) {
   const query = {
     estado: estado,
-    _id: { "$nin": idsUsados}
-  }
+    _id: { $nin: idsUsados },
+  };
   return CredenciaisAdvogados.findOne(query);
-}
+};
 
 credenciaisAdvogadosSchema.statics.criarHash = async function criarHash(obj) {
   let preHash = `${obj.login}${obj.senha}${obj.estado}`;
@@ -24,18 +30,22 @@ credenciaisAdvogadosSchema.statics.criarHash = async function criarHash(obj) {
   return Helper.hash(preHash);
 };
 
-credenciaisAdvogadosSchema.methods.salvar = async function salvar(){
+credenciaisAdvogadosSchema.methods.salvar = async function salvar() {
   this._hash = await CredenciaisAdvogados.criarHash(this);
   let credenciais = this.toObject();
   delete credenciais['_id'];
 
-  return await CredenciaisAdvogados.update(
-    {_hash: this._hash},
+  return await CredenciaisAdvogados.updateOne(
+    { _hash: this._hash },
     credenciais,
     { upsert: true }
-  )
-}
+  );
+};
 
-const CredenciaisAdvogados = mongoose.model('CredenciaisAdvogados', credenciaisAdvogadosSchema, 'credenciaisAdvogados');
+const CredenciaisAdvogados = mongoose.model(
+  'CredenciaisAdvogados',
+  credenciaisAdvogadosSchema,
+  'credenciaisAdvogados'
+);
 
 module.exports.CredenciaisAdvogados = CredenciaisAdvogados;

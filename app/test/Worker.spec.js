39e7adca621fd643986b30f60da90b943c8513be
Vitor Nascimento <vitor.gan21@gmail.com>
2020-07-31 17:18:55 -0300

    Adicionando 3 instancia

--- app/test/Worker.spec.js
+++ app/test/Worker.spec.js
@@ -1,11 +1,10 @@
 const { PeticaoTJSP } = require('../extratores/PeticaoTJSP');
 
 describe('Worker', async () => {
-  // const x = await new PeticaoTJSP({headless: false}).extrair('1011131-88.2018.8.26.0562', 1);
-  const x = await new PeticaoTJSP({ headless: false }).extrair(
-    '0000261-83.2017.8.26.0546',
-    2
-  );
+  const x = await new PeticaoTJSP({debug: true, headless: false}).extrair('1011131-88.2018.8.26.0562', 1);
+  // const x = await new PeticaoTJSP({ headless: false }).extrair('0000261-83.2017.8.26.0546', 2);
+  // const x = await new PeticaoTJSP({ headless: false }).extrair('1000907-79.2019.8.26.0102', 3);
+  delete x['logs'];
   console.table(x);
   console.log('fim');
 });

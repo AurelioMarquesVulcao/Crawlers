6ed95ef426423b0fded798976c3647f43a35c839
Vitor Nascimento <vitor.gan21@gmail.com>
2020-08-10 16:46:33 -0300

    Descomentado resposta ao big data e add finally

--- app/workers/TJSP/extracao.js
+++ app/workers/TJSP/extracao.js
@@ -5,6 +5,7 @@ const { ExtratorFactory } = require('../../extratores/extratorFactory');
 const { Extracao } = require('../../models/schemas/extracao');
 const { Helper, Logger } = require('../../lib/util');
 const { LogExecucao } = require('../../lib/logExecucao');
+const sleep = require('await-sleep');
 
 const logarExecucao = async (execucao) => {
   await LogExecucao.salvar(execucao);
@@ -25,6 +26,7 @@ const logarExecucao = async (execucao) => {
   new GerenciadorFila().consumir(nomeFila, async (ch, msg) => {
     const dataInicio = new Date();
     let message = JSON.parse(msg.content.toString());
+    console.table(message);
     let logger = new Logger('info', 'logs/OabTJSP/OabTJSPInfo.log', {
       nomeRobo: `${enums.tipoConsulta.Oab}.${enums.nomesRobos.TJSP}`,
       NumeroOab: message.NumeroOab,
@@ -48,16 +50,16 @@ const logarExecucao = async (execucao) => {
       );
       logger.info('Resultado da extracao salva');
 
-      // logger.info('Enviando resposta ao BigData');
-      // await Helper.enviarFeedback(
-      //   extracao.prepararEnvio()
-      // ).catch((err) => {
-      //   console.log(err);
-      //   throw new Error(
-      //     `OabTJSP - Erro ao enviar resposta ao BigData - Oab: ${message.NumeroOab}`
-      //   );
-      // });
-      // logger.info('Resposta enviada ao BigData');
+      logger.info('Enviando resposta ao BigData');
+      await Helper.enviarFeedback(
+        extracao.prepararEnvio()
+      ).catch((err) => {
+        console.log(err);
+        throw new Error(
+          `OabTJSP - Erro ao enviar resposta ao BigData - Oab: ${message.NumeroOab}`
+        );
+      });
+      logger.info('Resposta enviada ao BigData');
       logger.info('Finalizando processo');
       await logarExecucao({
         Mensagem: message,
@@ -67,9 +69,7 @@ const logarExecucao = async (execucao) => {
         logs: logger.logs,
         NomeRobo: enums.nomesRobos.TJSP,
       });
-      logger.info('Reconhecendo mensagem ao RabbitMQ');
-      ch.ack(msg);
-      logger.info('Mensagem reconhecida');
+
     } catch (e) {
       logger.info('Encontrado erro durante a execução');
       logger.log('error',e);
@@ -84,9 +84,12 @@ const logarExecucao = async (execucao) => {
         logs: logger.logs,
         NomeRobo: enums.nomesRobos.TJSP,
       });
+    } finally {
       logger.info('Reconhecendo mensagem ao RabbitMQ');
       ch.ack(msg);
       logger.info('Mensagem reconhecida');
+      console.log('\n\n\n\n');
+      await sleep(2000);
     }
   });
 })();

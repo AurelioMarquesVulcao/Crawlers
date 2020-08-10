218683c8f0edce916300886373d0c9b6a7f4e1e7
Vitor Nascimento <vitor.gan21@gmail.com>
2020-08-10 18:27:26 -0300

    Tratamento do arquivo de documento

--- app/workers/TJSP/extracaoPeticao.js
+++ app/workers/TJSP/extracaoPeticao.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const sleep = require('await-sleep');
 
+const fs = require('fs');
 const { enums } = require('../../configs/enums');
 const { GerenciadorFila } = require('../../lib/filaHandler');
 const { ExtratorFactory } = require('../../extratores/extratorFactory');
@@ -27,6 +28,7 @@ const logarExecucao = async (execucao) => {
   new GerenciadorFila().consumir(nomeFila, async (ch, msg) => {
     const dataInicio = new Date();
     let message = JSON.parse(msg.content.toString());
+    let arquivoPath;
     console.table(message);
     let logger = new Logger('info', 'logs/PeticaoTJSP/PeticaoTJSPInfo.log', {
       nomeRobo: `${enums.tipoConsulta.Peticao}.${enums.nomesRobos.TJSP}`,
@@ -43,25 +45,28 @@ const logarExecucao = async (execucao) => {
       );
       logger.logs = [...logger.logs, ...resultadoExtracao.logs];
       logger.info('Processo extraido');
-      let extracao = await Extracao.criarExtracao(
+      await Extracao.criarExtracao(
         message,
         resultadoExtracao,
         message.SeccionalOab
       );
       logger.info('Resultado da extracao salva');
 
+      logger.info('Preparando arquivo para upload');
+      arquivoPath = `./temp/peticoes/tjsp/${message.NumeroProcesso}.pdf`;
+      let data = fs.readFileSync(arquivoPath).toString('base64');
+      logger.info('Arquivo preparado');
+
       logger.info('Enviando resposta ao BigData');
-      await Helper.enviarFeedback(
-        extracao.prepararEnvio(),
-        {
-          NumeroDoProcesso: resultadoExtracao.numeroProcesso,
-        }
+      await Helper.feedbackDocumentos(
+        {ArquivoB64: data, NumeroProcesso: message.NumeroProcesso, Instancia: message.Instancia}
       ).catch((err) => {
         console.log(err);
         throw new Error(
-          `PeticaoTJSP - Erro ao enviar resposta ao BigData - Oab: ${message.NumeroOab}`
+          `PeticaoTJSP - Erro ao enviar resposta ao BigData - Processo: ${message.Processo}`
         );
       });
+
       logger.info('Resposta enviada ao BigData');
       logger.info('Finalizando processo');
       await logarExecucao({
@@ -72,7 +77,6 @@ const logarExecucao = async (execucao) => {
         logs: logger.logs,
         NomeRobo: 'PeticaoTJSP',
       });
-
 
     } catch (e) {
       logger.info('Encontrado erro durante a execução');
@@ -91,6 +95,12 @@ const logarExecucao = async (execucao) => {
 
 
     } finally {
+      if (fs.existsSync(arquivoPath)){
+        logger.info('Apagando arquivo temporario');
+        await fs.unlinkSync(arquivoPath);
+        logger.info('Arquivo apagado')
+      }
+
       logger.info('Reconhecendo mensagem ao RabbitMQ');
       ch.ack(msg);
       logger.info('Mensagem reconhecida');

8c1cf0f54bd1e00c62fdbabd096d55e238429c71
Newton Gonzaga Costa <ncosta@proadv.adv.br>
2020-08-12 14:36:41 -0300

    endpoint de consulta e enfileiramento

--- app/api/routes/bigdata.js
+++ app/api/routes/bigdata.js
@@ -5,8 +5,9 @@ const { ProcessoController } = require("../controller/processoController");
 // router.get('/', ProcessoController.contarDocumentos);
 router.get('/processo/:numeroProcesso', ProcessoController.obterProcesso);
 router.get('/processo/:numeroProcesso/andamentos', ProcessoController.obterAndamentos);
-router.get('/processos', ProcessoController.obterProcessoSolr);
+router.get('/processos', ProcessoController.obterProcessos);
 router.get('/processo-solr/:numeroProcesso', ProcessoController.obterProcessoSolr);
 router.get('/andamentos-solr/:numeroProcesso', ProcessoController.obterAndamentosSolar);
+router.post('/enfileirar-iniciais', ProcessoController.enfileirarDocumentos);
 
 module.exports = router;

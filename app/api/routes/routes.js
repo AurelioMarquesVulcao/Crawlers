12e0bf1eecfc082fa390de3646bc7e8b211f4ccc
Newton Gonzaga Costa <ncosta@proadv.adv.br>
2020-08-21 18:07:17 -0300

    endpoint para busca do consumo e contagem do consumo

--- app/api/routes/routes.js
+++ app/api/routes/routes.js
@@ -2,11 +2,13 @@ const router = require("express").Router();
 const bigdataRoutes = require("./bigdata");
 const feedbackRoutes = require("./feedback");
 const mockRoutes = require("./mock");
+const captchaRoutes = require("./captcha");
 const { ProcessoController } = require("../controller/processoController");
 
 router.get('/', ProcessoController.contarDocumentos);
 router.use('/bigdata', bigdataRoutes);
 router.use('/feedback', feedbackRoutes);
 router.use('/mock', mockRoutes);
+router.use('/captcha', captchaRoutes);
 
 module.exports = router;

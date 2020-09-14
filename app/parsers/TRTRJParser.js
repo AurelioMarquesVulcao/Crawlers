e7d89d6bdfdf703985e82b731be8cf32581756f1
aurelio <amarques@impacta.adv.br>
2020-09-14 16:31:36 -0300

    Parse da capa concluido

--- app/parsers/TRTRJParser.js
+++ app/parsers/TRTRJParser.js
@@ -24,10 +24,10 @@ class TRTParser extends BaseParser {
         let dadosAndamento;
 
         let dadosProcesso = new Processo({
-            capa: this.capa(),
+            capa: this.capa(extracao),
             oabs: "",
             qtdAndamentos: "",
-            origemExtracao: "",
+            origemExtracao: "TRT-RJ",
             envolvidos: "",
             detalhes: "",
         })
@@ -37,25 +37,48 @@ class TRTParser extends BaseParser {
             andamentos: dadosAndamento
         }
     }
-    capa() {
+    capa(extracao) {
+        let regex = this.regexVaraComarca(extracao.orgaoJulgador)
         let capa = {
-            uf: "",
-            comarca: "",
-            vara: "",
-            fase: "",
-            assunto: "",
-            classe: "",
-            dataDistribuicao: "",
-            instancia: "",
+            uf: "RJ",
+            comarca: regex[2],
+            vara: regex[1],
+            fase: extracao.itensProcesso[0].instancia,
+            assunto: [extracao.assuntos[0].descricao],
+            classe: extracao.classe,
+            dataDistribuicao: extracao.distribuidoEm,
+            instancia: extracao.itensProcesso[0].instancia,
+            segredoJustica: extracao.segredoJustica,
+            justicaGratuita: extracao.justicaGratuita,
+            valor: extracao.valorDaCausa
         }
 
         return capa
     }
+    regexVaraComarca(str) {
+        let regex = /\s*(.+?D[EO].+?)\s*D[EOA]\s*(.+)\s*/gim;
+        let m;
+        let resultado = []
+
+        while ((m = regex.exec(str)) !== null) {
+            // This is necessary to avoid infinite loops with zero-width matches
+            if (m.index === regex.lastIndex) {
+                regex.lastIndex++;
+            }
+
+            // The result can be accessed through the `m`-variable.
+            m.forEach((match, groupIndex) => {
+                resultado.push(`${match}`)
+                // console.log(`Found match, group ${groupIndex}: ${match}`);
+            });
+        }
+        //console.log(resultado);
+        return resultado
+    }
 }
 (async () => {
-    console.log(
-        new TRTParser().parse(Extracao)
-    );
-    
+    console.log(new TRTParser().parse(Extracao));
+    console.log(new TRTParser().parse(Extracao).processo.capa.assunto);
+    // console.log(Extracao.assuntos[0].descricao);
 
 })()

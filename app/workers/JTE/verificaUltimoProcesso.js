e2c7060d308f95c4bbc74653c6cddcf61a122d55
Aurelio <amarque@impacta.adv.br>
2020-10-14 13:32:12 -0300

    Criando verificador de status.

--- app/workers/JTE/verificaUltimoProcesso.js
+++ app/workers/JTE/verificaUltimoProcesso.js
@@ -2,9 +2,20 @@ const Axios = require('axios');
 const Fs = require('fs');
 const Path = require('path');
 const async = require('async');
+const mongoose = require('mongoose');
+const { enums } = require('../../configs/enums');
 
 const { GerenciadorFila } = require("../../lib/filaHandler");
+const { statusEstadosJTE } = require("../../models/schemas/jte")
 const rabbit = new GerenciadorFila();
+
+mongoose.connect(enums.mongo.connString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on('error', (e) => {
+  console.log(e);
+});
 
 
 const red = '\u001b[31m';
@@ -20,61 +31,63 @@ class Busca {
   static async buscaBigdata(tribunal) {
     let url = "http://172.16.16.3:8083/processos/obterUltimosSequenciaisPorUnidade";
     let resultado;
-    // try {
-    //   await Axios({
-    //     url: `${url}/?orgao=5&tribunal=${tribunal}&ano=2020`,
-    //     method: 'post',
-    //     maxContentLength: Infinity,
-    //     maxBodyLength: Infinity,
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //       'x-api-key': 'tk3TqbruYqJdFdW5fqctsurkNcZi5UHIVWUfiWfM7Xw',
-    //     },
-    //   })
-    //     .then((res) => {
-    //       resultado = res;
-    //       // console.log(resultado);
-    //     })
-    //     .catch((err) => {
-    //       console.log(err.config);
-    //       console.log(err.response.data);
-    //       throw err;
-    //     });
-    // } catch (e) {
-    //   console.log(red + "Falhou" + reset);
-    // }
-    resultado = [
-      {
-        "Orgao": 5,
-        "Tribunal": 1,
-        "UnidadeOrigem": 35,
-        "UltimoSequencial": 100765
-      },
-      {
-        "Orgao": 5,
-        "Tribunal": 1,
-        "UnidadeOrigem": 343,
-        "UltimoSequencial": 101065
-      },
-      {
-        "Orgao": 5,
-        "Tribunal": 1,
-        "UnidadeOrigem": 23,
-        "UltimoSequencial": 100825
-      },
-      {
-        "Orgao": 5,
-        "Tribunal": 1,
-        "UnidadeOrigem": 72,
-        "UltimoSequencial": 100815
-      }]
+    try {
+      await Axios({
+        url: `${url}/?orgao=5&tribunal=${tribunal}&ano=2020`,
+        method: 'post',
+        maxContentLength: Infinity,
+        maxBodyLength: Infinity,
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': 'tk3TqbruYqJdFdW5fqctsurkNcZi5UHIVWUfiWfM7Xw',
+        },
+      })
+        .then((res) => {
+          resultado = res;
+          // console.log(resultado);
+        })
+        .catch((err) => {
+          console.log(err.config);
+          console.log(err.response.data);
+          throw err;
+        });
+    } catch (e) {
+      console.log(red + "Falhou" + reset);
+    }
+
+
+    // resultado = [
+    //   {
+    //     "Orgao": 5,
+    //     "Tribunal": 1,
+    //     "UnidadeOrigem": 35,
+    //     "UltimoSequencial": 100765
+    //   },
+    //   {
+    //     "Orgao": 5,
+    //     "Tribunal": 1,
+    //     "UnidadeOrigem": 343,
+    //     "UltimoSequencial": 101065
+    //   },
+    //   {
+    //     "Orgao": 5,
+    //     "Tribunal": 1,
+    //     "UnidadeOrigem": 23,
+    //     "UltimoSequencial": 100825
+    //   },
+    //   {
+    //     "Orgao": 5,
+    //     "Tribunal": 15,
+    //     "UnidadeOrigem": 1,
+    //     "UltimoSequencial": 815
+    //   }]
     return resultado
   }
   static async pegaEstado() {
     let resultados = [];
     let estado;
 
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < 2; i++) {
       console.log(`Pegando o estado ${i}, aguardado resposta.`);
       estado = await this.buscaBigdata(i);
       resultados.push(estado);
@@ -83,42 +96,89 @@ class Busca {
     return resultados
   }
   static async criaNumerosCNJ() {
-    let resultado;
+    let resultado = [];
     let estado;
     const estados = await this.pegaEstado();
     for (let i = 0; i < estados.length; i++) {
       estado = estados[i];
       for (let j = 0; j < estado.length; j++) {
-        this.organizaCNJ(estado[j].UltimoSequencial, estado[j].Tribunal, estado[j].UnidadeOrigem);
+        // resultado.push(this.organizaCNJ(estado[j].UltimoSequencial, estado[j].Tribunal, estado[j].UnidadeOrigem));
+        // console.log(await this.validaCNJ(this.organizaCNJ(estado[j].UltimoSequencial, estado[j].Tribunal, estado[j].UnidadeOrigem))); 
+        resultado.push(await this.validaCNJ(this.organizaCNJ(estado[j].UltimoSequencial, estado[j].Tribunal, estado[j].UnidadeOrigem)));
+        // console.log(this.organizaCNJ(estado[j].UltimoSequencial, estado[j].Tribunal, estado[j].UnidadeOrigem));
       }
     }
-    resultado = "teste01"
+    // resultado = "teste01"
     return resultado
   }
 
   static organizaCNJ(ultimoSequencial, Tribunal, unidadeOrigem) {
-    console.log(ultimoSequencial, Tribunal, unidadeOrigem);
-    let sequencial; // = ultimoSequencial.toString();
-    let tribunal; // = Tribunal.toString();
-    let origem; // = unidadeOrigem.toString();
-    
-    console.log(sequencial, tribunal, origem);
+    // console.log(ultimoSequencial, Tribunal, unidadeOrigem);
+    let sequencial = this.completaNumero(ultimoSequencial, "ultimoSequencial");
+    let tribunal = this.completaNumero(Tribunal, "Tribunal");
+    let origem = this.completaNumero(unidadeOrigem, "unidadeOrigem");
+    return `${sequencial}0020205${tribunal}${origem}`
   }
 
-  geraSequencial(sequencial){
-    sequencial = sequencial.toString();
-    if (sequencial.length < 7) {
-      let zero = 7 - sequencial.length
-    } else {
-      sequencial = ultimoSequencial.toString();
+  static completaNumero(numero, tipo) {
+    let teste;
+    if (tipo == "unidadeOrigem") {
+      teste = 4;
+    } else if (tipo == "Tribunal") {
+      teste = 2;
+    } else if (tipo == "ultimoSequencial") {
+      teste = 7;
     }
+
+    let resultado = ''
+    numero = numero.toString();
+    if (numero.length < teste) {
+      let zero = teste - numero.length
+      // console.log(zero);
+      for (let i = 0; i < zero; i++) {
+        resultado += "0"
+      }
+      resultado = resultado + numero
+    } else {
+      resultado = numero;
+    }
+    return resultado
   }
 
-  criaPost(numero) {
+  /**
+   * Verifica se o numero CNJ obtido é maior que o ultimo rapado pelo robô JTE
+   * @param {string} numero Recebe numero CNJ
+   */
+  static async validaCNJ(numero) {
+    const regex = /([0-9]{7})([0-9]{2})([0-9]{4})([0-9]{1})([0-9]{2})([0-9]{4})/;
+    let estadoNumero = numero.replace(regex, '$5');
+    let comarca = numero.replace(regex, '$6');
+    // console.log(estadoNumero);
+    // console.log(comarca);
+    let busca = await statusEstadosJTE.find({
+      "estadoNumero": estadoNumero,
+      "comarca": comarca,
+    })
+    // console.log(busca.numeroUltimoProcecesso);
+    let ultimoSequencial = busca[0].numeroUltimoProcecesso;
+    let sequencial = numero.slice(0, 7);
+    // console.log(ultimoSequencial.slice(0, 7));
+    if (sequencial > ultimoSequencial) {
+      return numero
+    } else {
+      return null
+    }
+
+
+  }
+
+  static criaPost(numero) {
+
+
     let post = `{"NumeroProcesso" : "${numero}","NovosProcessos" : true}`;
     return post
   }
-  async posta() {
+  static async posta() {
     await rabbit.enfileirarLoteTRT("nomeFila", "mensagens");
   }
   processArray(element) {
@@ -131,4 +191,5 @@ class Busca {
   // await Busca.buscaBigdata(2)
   // console.log(await Busca.buscaBigdata(2));
   console.log(await Busca.criaNumerosCNJ());
+  await mongoose.connection.close()
 })()

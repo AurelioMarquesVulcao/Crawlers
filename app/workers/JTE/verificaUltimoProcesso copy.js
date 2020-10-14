6bd6e9cb3149115666db6c34adebb5cb059578ed
Aurelio <amarque@impacta.adv.br>
2020-10-14 18:29:30 -0300

    Criando reprocessamento de processos não baixados

--- app/workers/JTE/verificaUltimoProcesso copy.js
+++ app/workers/JTE/verificaUltimoProcesso copy.js
@@ -25,6 +25,77 @@ const reset = '\u001b[0m';
 
 
 class Busca {
+  /**
+   * Envia para o Rabbit todos as comarcas que possuem numero desatualizado.
+   */
+  static async posta() {
+    let cnj = await this.criaNumerosCNJ();
+    await rabbit.enfileirarLoteTRT("ReprocessamentoJTE", cnj);
+  }
+
+  /**
+   * Recebe o ultimo processo de todas as comarcas
+   * @param {array} estados Array de estados, com todos os estados 
+   */
+  static async criaNumerosCNJ() {
+    const estados = await this.pegaEstado();
+    let resultado = [];
+    let estado;
+    let valido;
+    // laco com estados
+    for (let i = 0; i < estados.length; i++) {
+      estado = estados[i];
+      // laco com comarcas
+      for (let j = 0; j < estado.length; j++) {
+        console.log("Validando estados");
+        // Array com os numeros CJN ja construidos, prontos para serem consumidos na fila.
+        valido = await this.validaCNJ(this.organizaCNJ(estado[j].UltimoSequencial, estado[j].Tribunal, estado[j].UnidadeOrigem))
+        if (valido != null) {
+          resultado.push(this.criaPost(valido));
+        }
+      }
+    }
+    if(resultado.length<1){
+      console.log(blue+"Não obtive processos novos em minha busca"+reset);
+    }
+    return resultado
+  }
+
+
+  static criaPost(numero) {
+    let post = `{"NumeroProcesso" : "${numero}","NovosProcessos" : true}`;
+    return post
+  }
+  processArray(element) {
+    for (let i = 0; i < element.length; i++) {
+
+    }
+  }
+
+  /**
+   * Busca um estado por vez obtendo um 24 array's, cada array possui todas as comarcas do estado
+   */
+  static async pegaEstado() {
+    let resultados = [];
+    let estado;
+    let contador = 0;
+    for (let i = 1; i < 25; i++) {
+      console.log(`Pegando o estado ${i}, aguardado resposta.`);
+      try{
+        if(contador == 6){
+          i++
+          contador = 0;
+        }
+        estado = await this.buscaBigdata(i);
+      } catch(e){
+        contador ++
+        estado = await this.buscaBigdata(i);
+      }
+      resultados.push(estado);
+      console.log("Extração concluida!");
+    }
+    return resultados
+  }
 
   /**
    * Busca no banco no BigdataV2 todos os processos de uma comarca
@@ -64,60 +135,14 @@ class Busca {
     // console.log(resultado.length);
     return resultado
   }
+  
+  // ----------------------------------------------- Funções complementares -------------------------------------------
   /**
-   * Busca um estado por vez obtendo um 24 array's, cada array possui todas as comarcas do estado
+   * Cria um numero CNJ para consumo.
+   * @param {number} ultimoSequencial Ultimo numero sequencial obtido no BigData V2
+   * @param {number} Tribunal Numero que repre
+   * @param {number} unidadeOrigem 
    */
-  static async pegaEstado() {
-    let resultados = [];
-    let estado;
-    let contador = 0;
-
-    for (let i = 1; i < 25; i++) {
-      console.log(`Pegando o estado ${i}, aguardado resposta.`);
-      try{
-        if(contador == 4){
-          i++
-          contador = 0;
-        }
-        estado = await this.buscaBigdata(i);
-      } catch(e){
-        contador ++
-        estado = await this.buscaBigdata(i);
-      }
-
-      resultados.push(estado);
-      console.log("Extração concluida!");
-    }
-    return resultados
-  }
-
-  static async criaNumerosCNJ() {
-    let resultado = [];
-    let estado;
-    let valido;
-    const estados = await this.pegaEstado();
-    for (let i = 0; i < estados.length; i++) {
-      estado = estados[i];
-      for (let j = 0; j < estado.length; j++) {
-        console.log("Validando estados");
-        // resultado.push(this.organizaCNJ(estado[j].UltimoSequencial, estado[j].Tribunal, estado[j].UnidadeOrigem));
-        // console.log(await this.validaCNJ(this.organizaCNJ(estado[j].UltimoSequencial, estado[j].Tribunal, estado[j].UnidadeOrigem))); 
-        valido = await this.validaCNJ(this.organizaCNJ(estado[j].UltimoSequencial, estado[j].Tribunal, estado[j].UnidadeOrigem))
-        if (valido != null) {
-          resultado.push(this.criaPost(valido));
-          await this.posta(this.criaPost(valido))
-
-        }
-        // console.log(this.organizaCNJ(estado[j].UltimoSequencial, estado[j].Tribunal, estado[j].UnidadeOrigem));
-      }
-    }
-    // resultado = "teste01"
-    if(resultado.length<1){
-      console.log(blue+"Não obtive processos novos em minha busca"+reset);
-    }
-    return resultado
-  }
-
   static organizaCNJ(ultimoSequencial, Tribunal, unidadeOrigem) {
     // console.log(ultimoSequencial, Tribunal, unidadeOrigem);
     let sequencial = this.completaNumero(ultimoSequencial, "ultimoSequencial");
@@ -186,25 +211,12 @@ class Busca {
 
   }
 
-  static criaPost(numero) {
-    let post = `{"NumeroProcesso" : "${numero}","NovosProcessos" : true}`;
-    return post
-  }
-  static async posta(cnj) {
-    // let cnj = await this.criaNumerosCNJ();
 
-    await rabbit.enfileirarLoteTRT("ReprocessamentoJTE", cnj);
-  }
-  processArray(element) {
-    for (let i = 0; i < element.length; i++) {
-
-    }
-  }
 }
 (async () => {
   // await Busca.buscaBigdata(2)
   // console.log(await Busca.buscaBigdata(2));
-  await Busca.criaNumerosCNJ();
-  // await Busca.posta()
+  // await Busca.criaNumerosCNJ();
+  await Busca.posta()
   await mongoose.connection.close()
 })()

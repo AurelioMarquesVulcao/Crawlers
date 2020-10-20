002dff591c10698b5ebe11efc695d9a07a67e48c
brunogss <b.g.santos@outlook.com>
2020-10-20 19:12:36 -0300

    Teste de puppeteer

--- app/test/TesteProxy.js
+++ app/test/TesteProxy.js
@@ -22,7 +22,7 @@ describe('Test proxy', () => {
       .catch(done);
   });
 
-  it('Deve retornar a pagina carrega do Horário de Brasília através do puppeteer', async (done) => {
+  it('Deve retornar a pagina carrega do Horário de Brasília através do puppeteer', () => {
     const browser = await puppeter.launch({
       // args: [
       //   '--proxy-server=http://proadvproxy:C4fMSSjzKR5v9dzg@proxy-proadv.7lan.net:8182',
@@ -31,7 +31,7 @@ describe('Test proxy', () => {
 
     const page = await browser.newPage();
     try {
-      await page.goto('https://www.horariodebrasilia.org/');
+      await page.goto('https://www.horariodebrasilia.org/');g
     } catch (e) {
       await browser.close();
       done(e);

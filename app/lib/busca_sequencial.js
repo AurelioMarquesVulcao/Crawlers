a07247391650d8db916750dd8c873c8e84d819dc
Vitor Nascimento <vnascimento@impacta.adv.br>
2020-12-03 17:19:09 -0300

    Adcicionado agora pesquisa mais rapida pelas comarcas

---
+++ app/lib/busca_sequencial.js
@@ -0,0 +1,93 @@
+const MongoClient = require('mongodb').MongoClient;
+const moment = require('moment')
+const ObjectId = require('mongoose/lib/types/objectid');
+const CONNECTION_URL_BDV2 = 'mongodb://admin:admin@bigrj01mon01:19000,bigrj01mon02:19001,bigrj01mon03:19000/bigDataV2?authSource=admin&replicaSet=rsBigData&readPreference=primary&appname=MongoDB%20Compass&ssl=false'
+
+module.exports.buscar_sequencial = async (tribunalExplicito, ano) => {
+  let tribunal = tribunalFactory[tribunalExplicito];
+  return new Promise((resolve, reject) => {
+    {
+
+      if (!tribunal) throw new Error('Tribunal invalido');
+
+      if (!ano) throw new Error('Ano não informado');
+
+      MongoClient.connect(CONNECTION_URL_BDV2, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }, function(err, client) {
+        if (err) {
+          console.log(err);
+          throw err;
+        };
+
+        console.log('Conectado ao servidor');
+
+        const db = client.db('bigDataV2');
+
+        let comarcas = consultar(db)
+          .then(res => {
+            console.log(res);
+            client.close();
+            return res
+          }).catch( err => {
+            throw new Error(err);
+          });
+
+        resolve(comarcas);
+
+      })
+
+    }
+  })
+}
+
+let consultar = (db) => {
+  return new Promise((resolve, reject) => {
+    const collection = db.collection('processo');
+
+    let data = moment().subtract(1, 'month');
+    data = data.format('YYYY-MM-DD');
+
+    let OBJ_ID = ObjectId(Math.floor((new Date(data))/1000).toString(16) + "0000000000000000");
+
+    collection.aggregate([
+      {'$match': {
+          '_id': {'$gt': OBJ_ID},
+          'CnjDetalhes.Ano': 2020,
+          'CnjDetalhes.OrgaoJustica': 8,
+          'CnjDetalhes.Tribunal': 21,
+        }},
+      {
+        '$group': {
+          '_id': '$CnjDetalhes.UnidadeOrigem',
+          'UltimoSequencial': {'$max': '$CnjDetalhes.NumeroSequencial'},
+          'CNJ': {'$max': '$NumeroCNJ'}
+        }
+      },
+      {
+        '$project': {
+          '_id':0,
+          'UnidadeOrigem': '$_id',
+          'UltimoSequencial': 1,
+          'CNJ': 1
+        }
+      }])
+      .toArray(function(err, docs){
+        if (err) reject(err);
+        console.log('consulta bem sucedida');
+        resolve(docs)
+    })
+  })
+}
+
+const tribunalFactory = {
+  'TJRS': {
+    tribunal: 21,
+    orgao: 8
+  },
+  'TJSP': {
+    tribunal: 26,
+    orgao: 8
+  }
+}
--- app/scripts/Filas/enfileirar_comarcas.js
+++ app/scripts/Filas/enfileirar_comarcas.js
@@ -1,31 +1,69 @@
 const Comarca = require('../../models/schemas/comarcas');
 const { GerenciadorFila } = require('../../lib/filaHandler');
+const axios = require('axios');
 require('../../bootstrap');
+const { buscar_sequencial } = require('../../lib/busca_sequencial');
 
 let args = process.argv;
 
-const enfileirar_comarcas = async () => {
-  let tribunal = args[2];
-
-  let gf = new GerenciadorFila();
-  let comarcas = await Comarca.find({ Estado: tribunal.substring(tribunal.length - 2) });
-  let tam = comarcas.length;
-  // process.exit(0);
-  for (let i = 0; i < tam; i++) {
-    let msg = comarcas[i].toJSON();
-    let obj = comarcas[i].toObject();
-
-    console.log(`[${msg.Comarca}] [${msg.UltimoProcesso}] -> `);
-
-    if(obj.UltimoProcesso) {
-      gf.enviar(`comarcas.${tribunal}.extracao`, msg);
-
-      await new Comarca(obj).setStatus(1);
+const resgata_comarcas_bd = async (orgao, tribunal, ano) => {
+  let config = {
+    method: 'get',
+    url: `http://172.16.16.3:8083/processos/obterUnidadesOrigemPorTribunal/?orgao=${orgao}&tribunal=${tribunal}&ano=${ano}`,
+    headers: {
+      'x-api-key': 'tk3TqbruYqJdFdW5fqctsurkNcZi5UHIVWUfiWfM7Xw'
     }
-  }
+  };
+
+  await axios(config).then(res => res.data);
+
+}
+
+const atualizar_comarcas = async (orgao, tribunal) => {
+
+}
+
+const enfileirar_comarcas = async () => {
+  let trib = args[2];
+  let ano = args[3];
+
+  let comarcasBD = await buscar_sequencial(trib, ano);
+
+  comarcasBD = comarcasBD.map(comarca => {
+    let numero = comarca.CNJ;
+    numero = numero.replace(/(\d{7})(\d{2})(\d{4})(\d)(\d{2})(\d{4})/, '$1-$2.$3.$4.$5.$6');
+
+    return {
+      CNJ: numero,
+      UnidadeOrigem: comarca.UnidadeOrigem,
+      UltimoSequencial: comarca.UltimoSequencial
+    }
+  })
+
+  console.log('oi')
+
+  // let gf = new GerenciadorFila();
+  // let comarcas = await Comarca.find({ Estado: tribunal.substring(tribunal.length - 2) });
+  // let tam = comarcas.length;
+  // // process.exit(0);
+  // for (let i = 0; i < tam; i++) {
+  //   let msg = comarcas[i].toJSON();
+  //   let obj = comarcas[i].toObject();
+  //
+  //   console.log(`[${msg.Comarca}] [${msg.UltimoProcesso}] -> `);
+  //
+  //   if(obj.UltimoProcesso) {
+  //     gf.enviar(`comarcas.${tribunal}.extracao`, msg);
+  //
+  //     await new Comarca(obj).setStatus(1);
+  //   }
+  // }
   return true;
 };
 
 enfileirar_comarcas()
-  .then((res) => console.log('processo de enfileiramento terminado'))
+  .then((res) => {
+    console.log('Processo de enfileiramento terminado');
+    process.exit(0)
+  })
   .catch((e) => console.log(e));

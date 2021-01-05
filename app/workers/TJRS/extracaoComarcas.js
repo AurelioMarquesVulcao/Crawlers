96a2a11b8d8668c9d58bfd572d08592fc3a4572c
Vitor Nascimento <vnascimento@impacta.adv.br>
2021-01-05 11:54:26 -0300

    Adicionado modelo para tratar caso o processo possua senha

--- app/workers/TJRS/extracaoComarcas.js
+++ app/workers/TJRS/extracaoComarcas.js
@@ -93,6 +93,7 @@ async function extrairNumeros(message, ultimo) {
       false
     );
     sequencial = `${Number(ultimoNumero[0]) + count}`;
+    // console.log({ sequencial })
 
     let mod = CnjValidator.calcula_mod97(
       sequencial,
@@ -109,9 +110,18 @@ async function extrairNumeros(message, ultimo) {
     if (erroEncontrado && !extracao.sucesso) {
       return { continuar: false, ultimo: anterior, count: count - 1 };
     }
-    if (extracao.sucesso) anterior = numero;
-    erroEncontrado = !extracao.sucesso;
+    if (extracao.sucesso || extracao.detalhes === 'Senha necessaria') {
+      erroEncontrado = false;
+      anterior = numero;
+    }
 
+    // console.log({detalhes: extracao.detalhes});
+    // console.log({sucesso: extracao.sucesso});
+
+    if (!extracao.sucesso && (extracao.detalhes !== 'Senha necessaria'))
+      erroEncontrado = !extracao.sucesso;
+
+    // console.log({erroEncontrado})
     count++;
   } while (count < 5);
 

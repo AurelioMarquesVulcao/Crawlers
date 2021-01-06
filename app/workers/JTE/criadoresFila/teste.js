db03e5545679734497097ef7e95000508e1f9f09
Aurelio <amarque@impacta.adv.br>
2021-01-06 12:38:09 -0300

    Verificando salto de numeração em todos os estados

--- app/workers/JTE/criadoresFila/teste.js
+++ app/workers/JTE/criadoresFila/teste.js
@@ -22,11 +22,57 @@ var nomeFila = 'processo.JTE.extracao.novos.1';
 var desligado = desligar.worker;
 
 
+
 (async () => {
-  let processos = await Processo.find({ "detalhes.orgao": 5, "detalhes.tribunal": 24, "detalhes.origem": 4 })
-  let numeros = processos.map((x) => x.detalhes.numeroProcessoMascara)
-  for (let i = 0; i < numeros.length; i++) {
-    console.log(numeros[i]);
+  let estados = ["01", "02", "03", "04", "05", "06", "07", "08", '09', "10", "11", "12", "13", "14", "15", "16", "17", "18", '19', "20", "21", "22", "23", "24"];
+  for (let i = 0; i < estados.length; i++) {
+    await estado(estados[i]);
   }
-  // await statusEstadosJTE.deleteMany({"dataCriaçãoJTE" : null});
+
+
+  process.exit();
 })()
+
+async function estado(str) {
+  let estados = await statusEstadosJTE.find({
+    "estadoNumero": str
+  })
+  for (let i = 0; i < estados.length; i++) {
+    let comarca = estados[i].comarca;
+    // console.log(estados[i].comarca);
+    await rastreio(parseInt(str), comarca);
+  }
+}
+
+async function rastreio(tribunal, comarca) {
+  let cnj = [];
+  let processos = await Processo.find({
+    "detalhes.orgao": 5,
+    "detalhes.tribunal": tribunal,
+    "detalhes.origem": comarca,
+    "detalhes.ano": 2020,
+  })
+  // .limit(5)
+  numeros = processos.map((x) => {
+    return {
+      "sequencial": Cnj.processoSlice(x.detalhes.numeroProcessoMascara).sequencial,
+      "processo": x.detalhes.numeroProcessoMascara
+    }
+  })
+  numerosOrdem = numeros.sort((a, b) => {
+    if (b.sequencial < a.sequencial) {
+      return -1;
+    }
+  })
+
+
+  for (let i = 0; i < numerosOrdem.length - 1; i++) {
+    let diferenca = numerosOrdem[i].sequencial - (numerosOrdem[i + 1].sequencial);
+    // console.log(numerosOrdem[i].sequencial, (numerosOrdem[i + 1].sequencial));
+    if (diferenca >= 300) {
+      console.log(numerosOrdem.slice(0, 5));
+      console.log(numerosOrdem[i].sequencial, (numerosOrdem[i + 1].sequencial));
+      console.log(diferenca);
+    }
+  }
+}

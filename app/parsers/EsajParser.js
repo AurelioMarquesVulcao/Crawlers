78bcbe7c4589de4a33468a5529cdcb7dbfbab1a1
Vitor Nascimento <vnascimento@impacta.adv.br>
2021-02-01 15:25:44 -0300

    Alterado parser

--- app/parsers/EsajParser.js
+++ app/parsers/EsajParser.js
@@ -12,7 +12,7 @@ class EsajParser extends BaseParser {
     let capa = this.extrairCapa($);
     let detalhes = this.extrairDetalhes($);
     let envolvidos = this.extrairEnvolvidos($);
-    // let oabs = this.extrairOabs($);
+    let oabs = this.extrairOabs(envolvidos);
     let andamentos = this.extrairAndamentos(
       $,
       dataAtual,
@@ -21,8 +21,10 @@ class EsajParser extends BaseParser {
     let status = this.extrairStatus(andamentos);
     let isBaixa = this.extrairBaixa(status);
 
+    let metadados = this.extrairMetadados($);
+
     detalhes.instancia = 1;
-    capa.dataDistribuicao = this.extrairDataDistribuicao($, andamentos); //TODO dataDistribuicao
+    capa.dataDistribuicao = this.extrairDataDistribuicao($, andamentos);
 
     const processo = new Processo({
       capa,
@@ -33,11 +35,10 @@ class EsajParser extends BaseParser {
       origemExtracao: 'ProcessoTJMS',
       status,
       isBaixa,
+      metadados,
     });
 
     return { processo, andamentos };
-
-    console.log('teste');
   }
 
   /**
@@ -96,17 +97,44 @@ class EsajParser extends BaseParser {
     });
   }
 
+  /**
+   * Verifica se há oabs referentes aos advogados
+   * @param envolvidos
+   * @return {*}
+   */
+  extrairOabs(envolvidos) {
+    let oab = '';
+    let oabs = envolvidos.map((element) => {
+      if (element.tipo === 'Advogado') {
+        oab = re.exec(element.nome, re(/\((?<oab>\d+\w+)\)/));
+        if (oab) {
+          return oab.groups.oab;
+        } else {
+          return null;
+        }
+      }
+    });
+
+    return oabs.filter(Boolean);
+  }
+
+  /**
+   * Faz a extração da capa do processo
+   * @param {cheerio.load} $
+   * @return {{assunto: [string], classe: string, foro: string, vara: string}}
+   */
   extrairCapa($) {
     let classe = this.extrairClasse($);
     let assunto = this.extrairAssunto($);
     let foro = this.extrairForo($);
     let vara = this.extrairVara($);
+    let audiencia = this.extrairAudiencias($);
 
     return { classe, assunto, foro, vara };
   }
 
   extrairClasse($) {
-    return $('#classeProcesso').text().trim();
+    return $('#areaProcesso').text().trim();
   }
 
   extrairAssunto($) {
@@ -121,12 +149,24 @@ class EsajParser extends BaseParser {
     return $('#varaProcesso').text().trim();
   }
 
+  /**
+   * Verifica o numero do processo e retira os detalhes do numero
+   * @param {cheerio.load} $
+   * @return {*}
+   */
   extrairDetalhes($) {
     let numeroProcesso = $('#numeroProcesso').text().trim();
 
     return Processo.identificarDetalhes(numeroProcesso);
   }
 
+  /**
+   * Faz a extração da lista de andamentos
+   * @param {cheerio.load} $
+   * @param dataAtual
+   * @param numeroProcesso
+   * @return {[Andamento]}
+   */
   extrairAndamentos($, dataAtual, numeroProcesso) {
     let andamentos = [];
     let andamentosHash = [];
@@ -173,6 +213,107 @@ class EsajParser extends BaseParser {
     });
 
     return andamentos;
+  }
+
+  /**
+   * Verifica qual é o status do processo
+   * @param {[Andamento]} andamentos
+   * @return {string|string}
+   */
+  extrairStatus(andamentos) {
+    for (let i = 0, tam = andamentos.length; i < tam; i++) {
+      let andamento = andamentos[i];
+      let statusIndex = this.possiveisStatusBaixa.indexOf(andamento.descricao);
+      if (statusIndex !== -1) return this.possiveisStatusBaixa[statusIndex];
+    }
+
+    return 'Aberto';
+  }
+
+  /**
+   * Verifica se houve baixa no processo de acordo com o status
+   * @param {String} status
+   * @return {boolean}
+   */
+  extrairBaixa(status) {
+    return this.possiveisStatusBaixa.indexOf(status) !== -1;
+  }
+
+  extrairDataDistribuicao($, andamentos) {
+    let data;
+    let distribuicao;
+
+    distribuicao = $($(`tr:contains("Distribuição:")`)[1]).text().strip();
+    distribuicao = distribuicao.replace(
+      /(?<data>\d{2}\/\d{2}\/\d{4})\sàs\s(?<hora>\d{2}:\d{2})(.*)/gm,
+      '$1 $2'
+    );
+    distribuicao = distribuicao.replace(/\s\s+/gm, ' ');
+    if (/\d{2}\/\d{2}\/\d{4} \d{2}:\d{2}/.test(distribuicao)) {
+      distribuicao = /\d{2}\/\d{2}\/\d{4}/.exec(distribuicao)[0];
+      data = moment(distribuicao, 'DD/MM/YYYY').format('YYYY-MM-DD');
+      return data;
+    }
+
+    let tam = andamentos.length;
+    if (tam > 0) return andamentos[tam - 1].data;
+  }
+
+  /**
+   * Extrai as audiencias do processo
+   * @param {cheerio.load} $
+   * @return {*[]|{tipo: (*|jQuery), data: string}[]}
+   */
+  extrairAudiencias($) {
+    let data = $(
+      $($('[name="audienciasPlaceHolder"]').next('table').children()[0]).find(
+        'tr.fundoClaro > td'
+      )[0]
+    ).text();
+    let tipo = $(
+      $($('[name="audienciasPlaceHolder"]').next('table').children()[0]).find(
+        'tr.fundoClaro > td'
+      )[1]
+    ).text();
+    let audiencia;
+
+    if (/\d/.test(data) && /\w/.test(tipo)) {
+      data = data.strip();
+      data = moment(data, 'DD/MM/AAAA');
+
+      tipo = tipo.strip();
+
+      if (data > moment()) {
+        return [{ data: data.format('YYYY-MM-DD'), tipo }];
+      }
+    }
+
+    return [];
+  }
+
+  /**
+   * Verifica os metadados do processo
+   * @param {cheerio.load} $
+   * @return {Object}
+   */
+  extrairMetadados($) {
+    let metadados = [];
+    let juiz = this.extrairJuiz($);
+    let valorDaAcao = this.extrairValorDaAcao($);
+
+    metadados = { ...juiz, ...valorDaAcao };
+
+    return metadados;
+  }
+
+  extrairJuiz($) {
+    return { juiz: this.tratarTexto($('#juizProcesso').text().strip()) };
+  }
+
+  extrairValorDaAcao($) {
+    return {
+      valorDaAcao: this.tratarTexto($('#valorAcaoProcesso').text().strip()),
+    };
   }
 }
 

1f95bc891c3e512fb6bb977701384c64c047210f
Vitor Nascimento <vnascimento@impacta.adv.br>
2021-02-01 15:42:45 -0300

    Adicionado index

---
+++ app/models/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  Processo: require('./schemas/processo').Processo,
+  Andamento: require('./schemas/andamento').Andamento,
+  Comarca: require('./schemas/comarcas').Comarca,
+};

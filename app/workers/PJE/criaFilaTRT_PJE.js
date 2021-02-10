8b4b57b28e34bd8fa69119d10039d4a9e53dfa06
Aurelio <amarque@impacta.adv.br>
2021-02-10 11:00:20 -0300

    Incluido Gerador de Fluxo no gerado de fila PJE

--- app/workers/PJE/criaFilaTRT_PJE.js
+++ app/workers/PJE/criaFilaTRT_PJE.js
@@ -2,49 +2,67 @@ const mongoose = require('mongoose');
 const sleep = require('await-sleep');
 const desligado = require('../../assets/jte/horarioRoboTRTRJ.json');
 
-const { enums } = require('../../configs/enums');
-const { Processo } = require('../../models/schemas/processo');
-const { GerenciadorFila } = require("../../lib/filaHandler");
 const { CriaFilaJTE } = require('../../lib/criaFilaJTE');
+const { enums } = require('../../configs/enums');
+const { FluxoController } = require('../../lib/fluxoController');
 const { getFilas } = require('./get_fila');
-
+const { GerenciadorFila } = require('../../lib/filaHandler');
+const { Processo } = require('../../models/schemas/processo');
+const { ExecucaoConsulta } = require('../../models/schemas/execucao_consulta');
 
 class CriaFilaPJE {
   constructor() {
-    // this.robo = new Robo();
-    this.fila = "processo.PJE.atualizacao.01";
+    this.fila = 'processo.PJE.atualizacao.01';
     this.criafila = new CriaFilaJTE();
-    this.rabbit = new GerenciadorFila()
+    this.rabbit = new GerenciadorFila();
     this.mensagens = [];
-  };
+  }
   async worker() {
     await this.testaFila();
-
   }
   /**
    * Monta a os blocos de estados a serem enfileirados
    */
   async montaFila() {
-
     let data = new Date();
-    data = new Date()
-    data.setDate(data.getDate() - 1)
+    data = new Date();
+    data.setDate(data.getDate() - 1);
     console.log(data);
-    // process.exit()
     let estado = [];
-    console.log("Fila concluída. Iniciando criador de fila.");
+    console.log('Fila concluída. Iniciando criador de fila.');
+    
+    // Para realizar textes.
+    
+    // await ExecucaoConsulta.deleteMany({
+    //   NomeRobo: this.fila.toLowerCase(),
+    // });
+    
     for (let i = 1; i < 25; i++) {
       if (i != 15) {
-        await this.atualizaProcessosFila(0, i, data)[0]
-        await sleep(3000)
+        await this.atualizaProcessosFila(0, i, data)[0];
+        await sleep(3000);
       }
     }
-    await this.rabbit.enfileirarLoteTRT(this.fila, this.mensagens)
-    // this.mensagens = [];
-    await sleep(5000);
-    // 
-    await this.testaFila();
+    await ExecucaoConsulta.deleteMany({
+      NomeRobo: this.fila.toLowerCase(),
+    });
+    for (let ii = 0;ii<this.mensagens.length;ii++){
+      await FluxoController.cadastrarExecucao(
+        this.fila.toLowerCase(),
+        this.fila,
+        this.mensagens[ii]
+      );
+    }
+    
+    
 
+    // função descontinuada.
+    // await this.rabbit.enfileirarLoteTRT(this.fila, this.mensagens);
+
+    this.mensagens = [];
+    await sleep(5000);
+    // reinicio o ciclo de testar a fila.
+    await this.testaFila();
   }
 
   /**
@@ -52,26 +70,26 @@ class CriaFilaPJE {
    * enfileira 300 processos de cada estado.
    */
   async testaFila() {
-    console.log("Testando Fila");
+    console.log('Testando Fila');
     this.mensagens = [];
-    console.log("Quantidade de Mensagens é.:", this.mensagens.length);
-    let statusFila = false
+    console.log('Quantidade de Mensagens é.:', this.mensagens.length);
+    let statusFila = false;
     let rabbit = await getFilas();
     console.log(rabbit);
     if (rabbit.length > 0) {
       for (let i = 0; i < rabbit.length; i++) {
         if (rabbit[i].nome == this.fila) {
-          statusFila = true
+          statusFila = true;
         }
       }
     }
     if (!statusFila) {
-      console.log("A fila ainda não consumiu...");
+      console.log('A fila ainda não consumiu...');
       await sleep(60 * 1000);
       await this.testaFila();
     } else {
-      console.log("A fila consumiu...");
-      await this.montaFila()
+      console.log('A fila consumiu...');
+      await this.montaFila();
     }
   }
 
@@ -84,8 +102,9 @@ class CriaFilaPJE {
    */
   async atualizaProcessosFila(pulo, tribunal, data) {
     let data2 = new Date();
-    data2.setDate(data.getDate() - 10)
-    console.log(data2, "tribunal", tribunal);
+    // subtrai x dias da data de hoje para a busca de processos
+    data2.setDate(data.getDate() - 10);
+    console.log(data2, 'tribunal', tribunal);
     let busca;
     // let mensagens = [];
     let agregar = await Processo.aggregate([
@@ -94,102 +113,82 @@ class CriaFilaPJE {
           'detalhes.orgao': 5,
           'detalhes.tribunal': tribunal,
           'detalhes.ano': new Date().getFullYear(),
-          "origemExtracao": "JTE",
-          "capa.dataDistribuicao": {
-            '$lt': data,
-            '$gt': data2
+          origemExtracao: 'JTE',
+          'capa.dataDistribuicao': {
+            $lt: data,
+            $gt: data2,
             // '$gt': new Date('2020-10-01')
-          }
-        }
+          },
+        },
       },
       {
         $project: {
-          "detalhes.numeroProcesso": 1,
-          "capa.dataDistribuicao": 1,
-          "_id": 1
-        }
+          'detalhes.instancia': 1,
+          'detalhes.numeroProcesso': 1,
+          'capa.dataDistribuicao': 1,
+          _id: 1,
+        },
       },
       { $sort: { _id: -1 } },
-      { $limit: 100 }
+      { $limit: 100 },
     ]).skip(pulo);
     // console.log(agregar);
     // process.exit()
     for (let i = 0; i < agregar.length; i++) {
       busca = `${agregar[i]._id}`;
-      this.mensagens.push(this.criaPost(agregar[i].detalhes.numeroProcesso, busca, agregar[i].capa.dataDistribuicao));
+      this.mensagens.push(
+        this.criaPost(
+          agregar[i].detalhes.numeroProcesso,
+          busca,
+          agregar[i].capa.dataDistribuicao,
+          agregar[i].detalhes.instancia
+        )
+      );
     }
   }
 
-
-  // async enfileirarTRT_RJ(numero, busca) {
-  //     let regex = (/([0-9]{7})([0-9]{2})(2020)(5)(01)([0-9]{4})/g.test(numero))
-  //     let mensagem = this.criaPost(numero, busca);
-  //     await this.criafila.enviarMensagem(this.fila, mensagem);
-  //     console.log("Processo enfileirado para Download");
-  // }
-
   /**
-  * Cria um post para ser enviado como mensagem para o rabbit
-  * @param {string} numero Numero de Processo
-  * @param {string} busca Objeto Id para busca no MONGO-DB
-  * @return {string} return.: {"NumeroProcesso" : "00004618720205130032","NovosProcessos" : true, "_id" : "5f6cb256f0375ab99a7cf367"}
-  */
-  criaPost(numero, busca, dataDistribuicao) {
-    let busca2 = busca.toString()
-    let data = `${dataDistribuicao.getDate()}/${dataDistribuicao.getMonth() + 1}/${dataDistribuicao.getFullYear()}`;
-    let post = `{"NumeroProcesso" : "${numero}","NovosProcessos" : true, "_id" : "${busca2}", "dataDistribuicaoP.":"${data}"}`;
-    console.log(post);
-    return post
+   * Cria um post para ser enviado como mensagem para o rabbit
+   * @param {string} numero Numero de Processo
+   * @param {string} busca Objeto Id para busca no MONGO-DB
+   * @param {string} instancia Instância do processo.
+   * @return {string} return.: {"NumeroProcesso" : "00004618720205130032","NovosProcessos" : true, "_id" : "5f6cb256f0375ab99a7cf367"}
+   */
+  criaPost(numero, busca, dataDistribuicao, instancia) {
+    let busca2 = busca.toString();
+    let data = `${dataDistribuicao.getDate()}/${
+      dataDistribuicao.getMonth() + 1
+    }/${dataDistribuicao.getFullYear()}`;
+
+    let post = {
+      NumeroProcesso: numero,
+      NovosProcessos: true,
+      _id: busca2,
+      dataDistribuicao: data,
+      Instancia: instancia,
+      NomeRobo: this.fila.toLowerCase(),
+      ConsultaCadastradaId: null,
+    };
+    return post;
   }
-
-};
-
-// embaralhador de array, faz com que a ordem de consumo da fila mude 
-// para que não baixe apenas o mesmo estado toda vez que inicie a aplicação.
-function embaralha(lista) {
-  for (let indice = lista.length; indice; indice--) {
-    const indiceAleatorio = Math.floor(Math.random() * indice);
-    // guarda de um índice aleatório da lista
-    const elemento = lista[indice - 1];
-    lista[indice - 1] = lista[indiceAleatorio];
-    lista[indiceAleatorio] = elemento;
-  }
-}
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
-
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
 }
 
 (async () => {
-  mongoose.connect(enums.mongo.connString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  mongoose.connection.on('error', (e) => {
+  try {
+    mongoose.connect(enums.mongo.connString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    mongoose.connection.on('error', (e) => {
+      console.log(e);
+    });
+  } catch (e) {
+    process.exit();
+  }
+  try {
+    new CriaFilaPJE().worker();
+    // new CriaFilaPJE().atualizaProcessosFila(0, 2, new Date("2020-12-12"))
+  } catch (e) {
     console.log(e);
-  });
-  new CriaFilaPJE().worker()
-  // new CriaFilaPJE().atualizaProcessosFila(0, 2, new Date("2020-12-12")) 
-
-})()
+  }
+})();

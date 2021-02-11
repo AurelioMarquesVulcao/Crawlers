7397e60721f2ca944a93265e151a04fff12133eb
Aurelio <amarque@impacta.adv.br>
2021-02-11 15:08:35 -0300

    Ajuste da String para String de Produção

--- app/workers/JTE_DOC/Worker/extratorDoc.js
+++ app/workers/JTE_DOC/Worker/extratorDoc.js
@@ -133,8 +133,8 @@ async function worker(nomeFila) {
         for (let w = 0; w < 1; w++) {
           // Criando fila para Download de documentos
 
-          let local = '/home/aurelio/crawlers-bigdata/app/downloads';
-          // let local = '/app/downloads';
+          // let local = '/home/aurelio/crawlers-bigdata/app/downloads';
+          let local = '/app/downloads';
 
           if (tipo != 'HTML') {
             await new downloadFiles().download(nome, links, local);

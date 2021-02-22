c0670537450a2aa38bd53f24ee2f04c5d46ba099
Vitor Nascimento <vnascimento@impacta.adv.br>
2021-02-22 11:08:40 -0300

    Atualizado workers para funcionar com a reutilização de captcha e cookies

--- app/workers/TJCE/extracaoComarcas.js
+++ app/workers/TJCE/extracaoComarcas.js
@@ -3,8 +3,8 @@ const { enums } = require('../../configs/enums');
 const { GerenciadorFila } = require('../../lib/filaHandler');
 const sleep = require('await-sleep');
 const { CnjValidator } = require('../../lib/util');
-const extratores = require('../../extratores');
-const Comarca = require('../../models/schemas/comarcas');
+const { ProcessoTJCE } = require('../../extratores/ProcessoESAJ');
+const { Comarca } = require('../../models');
 const moment = require('moment');
 
 (async () => {
@@ -18,10 +18,12 @@ const moment = require('moment');
   });
 
   const nomeFila = 'comarcas.TJCE.extracao';
+  let execucaoAnterior = {};
 
   new GerenciadorFila().consumir(nomeFila, async (ch, msg) => {
     let message = JSON.parse(msg.content.toString());
-    let comarca = new Comarca(message);
+    let comarca = await Comarca.findOne({ _id: message._id });
+    comarca = new Comarca(comarca);
     console.table(message);
 
     if (!/\d{7}-\d{2}\.\d{4}\.\d\.\d{2}\.\d{4}/.test(message.UltimoProcesso)) {
@@ -39,9 +41,10 @@ const moment = require('moment');
     let processados = 0;
     let inicio = moment();
     do {
-      continuar = await extrairNumeros(message, ultimo);
+      continuar = await extrairNumeros(message, ultimo, execucaoAnterior);
       processados += continuar.count;
       ultimo = continuar.ultimo;
+      execucaoAnterior = continuar.execucaoAnterior;
       if (!continuar.continuar) break;
       // if (processados > 6) break
     } while (true);
@@ -61,7 +64,7 @@ const moment = require('moment');
     await comarca.setStatus(3);
 
     ch.ack(msg);
-    await sleep(200);
+    await sleep(650);
   });
 })();
 
@@ -81,9 +84,10 @@ const moment = require('moment');
  * @param message.Tribunal
  * @param message.UltimoProcesso
  * @param {boolean|string} ultimo
- * @returns {Promise<{ultimo: string, count: number, continuar: boolean}>}
+ * @param {Object} execucaoAnterior
+ * @returns {Promise<{ultimo: string, count: number, continuar: boolean, execucaoAnterior: Object}>}
  */
-async function extrairNumeros(message, ultimo) {
+async function extrairNumeros(message, ultimo, execucaoAnterior = {}) {
   console.log({ ultimo });
   let ultimoNumero = ultimo ? ultimo : message.UltimoProcesso;
   let anterior = ultimoNumero;
@@ -94,9 +98,14 @@ async function extrairNumeros(message, ultimo) {
   let extracao;
 
   ultimoNumero = ultimoNumero.split(/\D/g);
-
   do {
-    let extrator = new extratores.ProcessoTJCE();
+    console.log({
+      execucaoAnterior: Boolean(Object.keys(execucaoAnterior).length),
+    });
+    let extrator = new ProcessoTJCE(
+      'https://www.tjrs.jus.br/site_php/consulta/index.php',
+      false
+    );
     sequencial = `${Number(ultimoNumero[0]) + count}`;
 
     let mod = CnjValidator.calcula_mod97(
@@ -107,7 +116,8 @@ async function extrairNumeros(message, ultimo) {
     );
     numero = `${sequencial}-${mod}.${ultimoNumero[2]}.${ultimoNumero[3]}.${ultimoNumero[4]}.${ultimoNumero[5]}`;
     numero = ('000000' + numero).slice(-25);
-    extracao = await extrator.extrair(numero, null, 1);
+    extracao = await extrator.extrair(numero, null, 1, {}, execucaoAnterior);
+    execucaoAnterior = extracao.execucaoAnterior;
 
     // console.log({ count, sequencial,ultimoNumero, numero, mod })
 
@@ -122,6 +132,8 @@ async function extrairNumeros(message, ultimo) {
     // console.log({detalhes: extracao.detalhes});
     // console.log({sucesso: extracao.sucesso});
 
+    // console.log({ execucaoAnterior });
+
     if (!extracao.sucesso && extracao.detalhes !== 'Senha necessaria')
       erroEncontrado = !extracao.sucesso;
 
@@ -129,5 +141,5 @@ async function extrairNumeros(message, ultimo) {
     count++;
   } while (count < 5);
 
-  return { continuar: true, ultimo: numero, count };
+  return { continuar: true, ultimo: numero, count, execucaoAnterior };
 }
